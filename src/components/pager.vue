9873c81c104f7e8fc32e3ad52b13200e87580c12
seishi <185766516@qq.com>
2015-12-03 11:29:49 +0800

    添加分页交互

--- src/components/pager.vue
+++ src/components/pager.vue
@@ -1,6 +1,6 @@
 <template lang="jade">
   .pager.tar
-    button.pager-btn.pager-prev(:class="{'disabled': current === 1}")
+    button.pager-btn.pager-prev(:class="{'disabled': current === 1}", @click="current--")
       i.fa.fa-chevron-left
 
     .pager-container(v-if="pages < 10")
@@ -10,14 +10,30 @@
     .pager-container(v-if="pages >= 10 && ( current <= 3 || current >= pages - 2)")
       .pager-item(v-for="page in 3", :class="{'current': current === page + 1}",  @click="current = page + 1")
         span {{page + 1}}
-      .pager-item
-        span(@click="showInput = !showInput") ...
+      .pager-more
+        span(@click="toggleInput($event)") ...
         .pager-input(v-show="showInput")
-          input(type="number", max="{{pages}}")
+          input(type="number", max="{{pages}}", @change="onInput")
       .pager-item(v-for="offset in 3", :class="{'current': current === pages - 2 + offset}",  @click="current = pages - 2 + offset")
         span {{pages - 2 + offset}}
+
+    .pager-container(v-if="pages >= 10 && current > 3 && current < pages - 2")
+      .pager-item(:class="{'current': current === 1}",  @click="current = 1")
+        span 1
+      .pager-more
+        span(@click="toggleInput1($event)") ...
+        .pager-input(v-show="showInput1")
+          input(type="number", max="{{pages}}", @change="onInput")
+      .pager-item(v-for="offset in 3", :class="{'current': current === current - 1 + offset}",  @click="current = current - 1 + offset")
+        span {{current - 1 + offset}}
+      .pager-more
+        span(@click="toggleInput2($event)") ...
+        .pager-input(v-show="showInput2")
+          input(type="number", max="{{pages}}", @change="onInput")
+      .pager-item(:class="{'current': current === pages}",  @click="current = pages")
+        span {{pages}}
     //- input.pager-input(type="text")
-    button.pager-btn.pager-next(:class="{'disabled': current === pages}")
+    button.pager-btn.pager-next(:class="{'disabled': current === pages}", @click="current++")
       i.fa.fa-chevron-right
 </template>
 
@@ -31,6 +47,7 @@
 
     .pager-btn
     .pager-item
+    .pager-more
       display inline-block
       vertical-align middle
       box-sizing border-box
@@ -61,27 +78,30 @@
       vertical-align middle
 
     .pager-item
+    .pager-more
       position relative
       font-size 12px
-      border 1px solid #E5E5E5
       transition background .3s, color .3s, border-color .3s
 
       span
         display inline-block
         padding 0 8px
+        border 1px solid #E5E5E5
         height 26px
         line-height 24px
 
-      &:hover
-        border-color red
-        background red
-        color #FFF
-        cursor pointer
+        &:hover
+          border-color red
+          background red
+          color #FFF
+          cursor pointer
 
+    .pager-item
       &.current
-        border-color red
-        background #FFF
-        color red
+        span
+          border-color red
+          background #FFF
+          color red
 
     .pager-input
       absolute left 50% top -40px
@@ -111,12 +131,14 @@
     &.tal
       .pager-btn
       .pager-item
+      .pager-more
         margin-right 5px
 
     &.tar
     &.tac
       .pager-btn
       .pager-item
+      .pager-more
         margin-left 5px
 
 </style>
@@ -141,8 +163,10 @@
 
     data: function () {
       return {
-        showInput: false
-      }
+        showInput: false,
+        showInput1: false,
+        showInput2: false
+      };
     },
 
     computed: {
@@ -152,6 +176,41 @@
     },
 
     methods: {
+      toggleInput: function (evt) {
+        this.showInput = !this.showInput;
+        if (this.showInput) {
+          evt.target.parentNode.getElementsByTagName('input')[0].focus();
+        }
+      },
+
+      toggleInput1: function (evt) {
+        this.showInput1 = !this.showInput1;
+        if (this.showInput1) {
+          evt.target.parentNode.getElementsByTagName('input')[0].focus();
+        }
+      },
+
+      toggleInput2: function (evt) {
+        this.showInput2 = !this.showInput2;
+        if (this.showInput2) {
+          evt.target.parentNode.getElementsByTagName('input')[0].focus();
+        }
+      },
+
+      onInput: function (evt) {
+        var page = Number(evt.target.value);
+        // alert(typeof page === 'number');
+        if (typeof page === 'number') {
+          page = Math.floor(page);
+          this.showInput = false;
+          this.showInput1 = false;
+          this.showInput2 = false;
+          this.current = page;
+          evt.target.value = '';
+        } else {
+          alert('请输入数字')
+        }
+      }
     }
   };
 </script>
