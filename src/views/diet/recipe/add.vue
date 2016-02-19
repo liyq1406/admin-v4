af5023e83a82562ccaf4da7e1697731ce09f17e7
qiuguohui <530784837@qq.com>
2016-02-19 15:30:53 +0800

    添加过滤器

--- src/views/diet/recipe/add.vue
+++ src/views/diet/recipe/add.vue
@@ -6,32 +6,106 @@ section.main-wrap.diet
         i.fa.fa-arrow-circle-left
         | 菜谱管理
     .panel
-      .panel-hd(@click="showSelectFoods=true")
+      .panel-hd
         h2 添加菜谱
       .panel-bd
         .form
-          form
+          form(v-form, name="validation")
             .form-row
-              label.form-control 123:
+              label.form-control {{ $t("food.fields.name") }}:
               .controls
                 .input-text-wrap(v-placeholder="'请填写菜谱名称'")
-                  input.input-text(type="text",lazy,v-model="toAddrecipeObj.title")
-                .form-tips.form-tips-error(v-show="false")
-                  span 错误提示
+                  input.input-text(v-model="toAddrecipeObj.name",type="text", v-form-ctrl, name="name", maxlength="250", required, lazy)
+                .form-tips.form-tips-error(v-if="validation.$submitted && validation.name.$pristine")
+                  span(v-if="validation.name.$error.required") {{ $t('validation.required', {field: $t('food.fields.name')}) }}
+                .form-tips.form-tips-error(v-if="validation.name.$dirty")
+                  span(v-if="validation.name.$error.required") {{ $t('validation.required', {field: $t('food.fields.name')}) }}
+                  span(v-if="validation.name.$error.maxlength") {{ $t('validation.maxlength', [ $t('food.fields.name'), 250]) }}
+                  span(v-if="validation.name.$error.customValidator") {{ $t('validation.format', {field: $t('food.fields.name')}) }}
             .form-row
-              .prompt-box
-                |小提示：
-                br
-                |1、步骤图宽度在150像素至150像素；
-                br
-                |2、每个步骤用一段话描述，如果不需要可将内容留空；
+              label.form-control {{ $t("food.fields.images") }}:
+              .controls.controls-image
+                .image-uploader
+                  image-uploader(v-for="n in toAddrecipeObj.images.length", :image.sync="toAddrecipeObj.images[n]")
+                .form-tips 建议上传640像素*480像素成品图，最多不超过3张
+            .form-row
+              label.form-control 难度:
+              .controls
+                .select-group
+                  .select
+                    select(v-model="toAddrecipeObj.difficulties", v-form-ctrl, name="classification")
+                      option(v-for="difficulty in difficulties", :value="", :selected="$index===0") {{difficulty}}
+                    span.fa.fa-times
+            .form-row
+              label.form-control {{ $t("food.fields.classification") }}:
+              .controls
+                .select-group
+                  .select.clear(v-for="selectedclassification in toAddrecipeObj.classifications")
+                    select(@change="selectclassEvent(selectedclassification,$event)",v-form-ctrl, name="classification")
+                      option(v-for="classification in classifications", :value="", :selected="$index===0") {{classification}}
+                    span.fa.fa-times(@click="removeObj(selectedclassification,toAddrecipeObj.classifications)")
+                button.btn.btn-success(@click.prevent="addClassification")
+                  i.fa.fa-plus
+                  | 添加类别
+            .form-row
+              label.form-control {{ $t("food.fields.instructions") }}:
+              .controls
+                .input-text-wrap(v-placeholder="$t('food.placeholders.instructions')")
+                  textarea.input-text(v-model="toAddrecipeObj.instructions", type="text", v-form-ctrl, name="instructions", maxlength="250", lazy)
+                .form-tips.form-tips-error(v-if="validation.instructions.$dirty")
+                  span(v-if="validation.instructions.$error.maxlength")  {{ $t('validation.maxlength', [ $t('food.fields.instructions'), 250]) }}
+            .form-row
+              label.form-control 食材:
+              .controls
+                button.btn.btn-success(@click.prevent="showSelectFoods=true")
+                  i.fa.fa-plus
+                  | 添加食材
+            .form-row
+              label.form-control 烹饪设备:
+              .controls
+                .select-group(v-for="n in 3")
+                  .select.inline
+                    select(v-form-ctrl, name="classification")
+                      option(v-for="classification in classifications", :value="", :selected="$index===0") {{classification}}
+                  .input-text-wrap.inline
+                    input.input-text-time(type="text")
+                    span.text-time 分钟
+                  .delete-input.inline
+                    span.fa.fa-times
+                  .input-text-wrap.block.margin10(v-placeholder="'请输入设备烹饪指令'")
+                    textarea.input-text(v-model="toAddrecipeObj.instructions", type="text",lazy)
 
-              .step-box(v-for="n in 4")
-                label.form-control 第{{n+1}}步
-                .imgInput
-                  .selectedImg
-                    input(type="file")
-                .step-text 123
+                button.btn.btn-success
+                  i.fa.fa-plus
+                  | 添加烹饪设备
+            .form-row
+              label.form-control 步骤:
+              .controls
+                .prompt-box
+                  |小提示：
+                  br
+                  |1、步骤图宽度在150像素至150像素；
+                  br
+                  |2、每个步骤用一段话描述，如果不需要可将内容留空；
+
+                .step-box(v-for="cookingstep in cookingSteps")
+                  label.form-control 第{{$index+1}}步:
+                  .controls.controls-image
+                    .image-uploader
+                      image-uploader(:image.sync="cookingstep.images")
+                  .input-text-wrap.step-text
+                    textarea.input-text(v-model="cookingstep.description",type="text",lazy,placeholder="请填写步骤的描述")
+                  .button-list
+                    .controlButton.button-up(v-show="cookingSteps.length>1&&$index>0",@click="stepEvent(cookingstep,$index,'up')")
+                      i.icon.fa.fa-long-arrow-up
+                    .controlButton.button-down(v-show="cookingSteps.length>1&&$index<(cookingSteps.length-1)",@click="stepEvent(cookingstep,$index,'down')")
+                      i.icon.fa.fa-long-arrow-down
+                    .controlButton.button-add(@click="stepEvent(cookingstep,$index,'add')")
+                      i.icon.fa.fa-plus
+                    .controlButton.button-del(v-show="cookingSteps.length>1",@click="stepEvent(cookingstep,$index,'del')")
+                      i.icon.fa.fa-times
+            .form-actions
+              button.btn.btn-primary.btn-lg(type="submit") {{ $t("common.save") }}
         modal(:show.sync="showSelectFoods",:width="800")
           h3(slot="header") 选择食材
           .food-box(slot="body")
@@ -66,7 +140,7 @@ section.main-wrap.diet
                     tr(v-for="selectedFood in selectedFoodList")
                       td
                         span {{selectedFood.name}}
-                        i.fa.fa-times-circle(@click="delectFood(selectedFood)")
+                        i.fa.fa-times-circle(@click="deleteFood(selectedFood)")
             .button-box
               .form-actions
                 button.btn.btn-default(@click.prevent.stop="showSelectFoods=false") {{ $t("common.cancel") }}
@@ -78,6 +152,7 @@ import Modal from '../../../components/modal.vue';
 import Pager from '../../../components/pager.vue';
 import Select from '../../../components/select.vue';
 import SearchBox from '../../../components/search-box.vue';
+import ImageUploader from '../../../components/image-uploader.vue';
 
 export default {
   name: 'AddForm',
@@ -86,15 +161,23 @@ export default {
     'v-select': Select,
     'search-box': SearchBox,
     'pager': Pager,
-    'modal': Modal
+    'modal': Modal,
+    'image-uploader': ImageUploader
   },
   data () {
     return {
+      validation: {},
       adding: false,
       showSelectFoods: false,
       toAddrecipeObj: {
-        title: ''
+        name: '',
+        classifications: [{main: '湘菜', sub: ''}, {main: '粤菜', sub: ''}],
+        difficulties: [],
+        images: ['', '', ''],
+        instructions: ''
       },
+      difficulties: ['不限', '新手', '初级', '中极', '高级', '厨神'],
+      classifications: ['测试菜', '粤菜', '湘菜', '东北菜', '养生'],
       category: '全部',
       categoryOptions: [
         {label: '全部', value: '全部'},
@@ -130,6 +213,26 @@ export default {
         }
       ],
       selectedFoodList: [
+      ],
+      cookingSteps: [
+        {
+          index: 0,
+          description: '',
+          time: 30,
+          images: ''
+        },
+        {
+          index: 0,
+          description: '',
+          time: 30,
+          images: ''
+        },
+        {
+          index: 1,
+          description: '',
+          time: 30,
+          images: ''
+        }
       ]
     };
   },
@@ -140,12 +243,62 @@ export default {
   },
 
   methods: {
+    selectclassEvent (obj, event) {
+      console.log(event.target.value);
+      obj.main = event.target.value;
+    },
+    addClassification () {
+      var self = this;
+      var noRepeatName = function () {
+        for (var i = 0; i < self.classifications.length; i++) {
+          console.log(self.toAddrecipeObj.classifications[0]);
+          for (var j = 0; j < self.toAddrecipeObj.classifications.length; i++) {
+            if (self.toAddrecipeObj.classifications[j].main !== self.classifications[i]) {
+
+              return self.classifications[i];
+            }
+          }
+        }
+      };
+      console.log(noRepeatName());
+      var newclass = {
+        main: noRepeatName(),
+        sub: ''
+      };
+      self.toAddrecipeObj.classifications.push(newclass);
+    },
+    stepEvent (step, index, eventType) {
+      var self = this;
+      var newstep = {
+        index: 0,
+        description: '',
+        time: 0,
+        images: ''
+      };
+      if (eventType === 'up') {
+        console.log('up');
+        self.cookingSteps.splice(index, 1);
+        self.cookingSteps.splice(index - 1, 0, step);
+      }else if (eventType === 'down') {
+        console.log('down');
+        self.cookingSteps.splice(index, 1);
+        self.cookingSteps.splice(index + 1, 0, step);
+      }else if (eventType === 'add') {
+        self.cookingSteps.splice(index + 1, 0, newstep);
+      }else if (eventType === 'del') {
+        self.cookingSteps.$remove(step);
+      }
+    },
+    removeObj (obj, arr) {
+      console.log(123);
+      arr.$remove(obj);
+    },
     addFood (food) {
       var self = this;
       self.selectedFoodList.push(food);
       self.updateToSelectedList();
     },
-    delectFood (food) {
+    deleteFood (food) {
       var self = this;
       self.selectedFoodList.$remove(food);
       self.updateToSelectedList();
@@ -180,48 +333,97 @@ export default {
 
 <style lang="stylus">
   @import '../../../assets/stylus/common'
+
+  .inline
+    display inline-block
+  .block
+    display block
+  .clear
+    clear both
   .diet
     .panel
       .panel-bd
         .form-row
           max-height 5000px
           .prompt-box
-            width 96%
+            width 100%
             height auto
-            margin 20px auto
+            margin 0 auto 10px
             border 1px solid #ccc
+            box-sizing border-box
             padding 10px
           .step-box
             margin-bottom 20px
+            width 100%
+            position relative
             clearfix()
             label.form-control
+              float left
               text-align right
+              width 70px
               line-height 120px
               padding-right 20px
               box-sizing border-box
-            .imgInput
+            .controls-image
               float left
-              padding-top 10px
-              .selectedImg
-                position relative
-                float left
-                width 100px
-                height 100px
-                background red
-                margin 0 20px
-                input
-                  position absolute
-                  left 0
-                  top 0
-                  opacity 0.5
-                  width 100%
-                  height 100%
-
+              width 182px
+              min-width 182px
+              overflow hidden
             .step-text
               float left
-              width 60%
+              width 50%
+              box-sizing border-box
               height 120px
-              border 1px solid #ccc
+              .input-text
+                height 100%
+            .button-list
+              position absolute
+              left 100.5%
+              height 120px
+              opacity 0
+              /*transition opacity ease 0.3s*/
+              .controlButton
+                height 25px
+                width 25px
+                line-height 25px
+                margin-bottom 6px
+                background #999
+                text-align center
+                cursor pointer
+                &:hover
+                  background red
+                i.icon
+                  color #fff
+            &:hover
+              .button-list
+                opacity 1
+          .controls
+            width 70%
+            .select-group
+              margin-bottom 20px
+              position relative
+              .margin10
+                margin-top 10px
+                margin-bottom 10px
+              .input-text-wrap
+                input.input-text-time
+                  border 1px solid #d9d9d9
+                  display inline-block
+                  width 60px
+                  box-sizing border-box
+                  font-size 14px
+                  height 38px
+                  line-height 38px
+                  padding 6px 20px
+                  margin-right 10px
+                span.text-time
+                  margin-right 10px
+              .delete-input
+                position absolute
+                right 5px
+                top 5px
+                span.fa
+                  color #c0252e
       .modal
         .modal-body
           clearfix()
