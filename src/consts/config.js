801154fa46d1317085b2b734fdfe3fe6e1c9cac1
seishi <185766516@qq.com>
2016-03-18 14:35:04 +0800

    修改测试 API 地址

--- src/consts/config.js
+++ src/consts/config.js
@@ -1,7 +1,7 @@
 
 export default {
   // 测试服务器
-  apiRoot: 'http://42.121.122.228:8887/v2',
+  apiRoot: 'http://api-test.xlink.cn:8887/v2',
   // 开发本地
   // apiRoot: 'http://localhost:8887/v2',
   // 海曼
@@ -9,7 +9,7 @@ export default {
   // 正式地址
   // apiRoot: 'https://api2.xlink.cn:443/v2',
   // 测试服务器v1
-  apiRootV1: 'http://42.121.122.228:8887/v1',
+  apiRootV1: 'http://api-test.xlink.cn:8887/v1',
   // 正式地址v1
   // apiRootV1: 'https://api2.xlink.cn:443/v1',
   recipeApiRoot: 'http://plugin.xlink.cn/39352b6f8d224064832bcec9b96958d1',
