ee7816ef9aa1e2bf3c5c6c59d11953e07200c93b
FulukawaA <442873864@qq.com>
2016-04-14 16:55:20 +0800

    设备参数-数据监控&故障记录

--- src/views/product/device/params/data-monitoring.vue
+++ src/views/product/device/params/data-monitoring.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="panel">
     <div class="panel-bd">
-      数据监控
+      <div class="chartwrap">
+        <div id="trendChart" style="height:480px"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import { globalMixins } from '../../../../mixins'
+  import echarts from 'echarts/echarts'
+  require('echarts/chart/line')
 
   export default {
     name: 'BasicInfo',
@@ -18,14 +22,102 @@
 
     data () {
       return {
+        dates: [
+          '3-01',
+          '3-02',
+          '3-03',
+          '3-04',
+          '3-05',
+          '3-06',
+          '3-07'
+        ],
+        environmentTems: [
+          '19',
+          '25',
+          '15',
+          '35',
+          '22',
+          '16',
+          '10'
+        ],
+        waterboxTems: [
+          '48',
+          '42',
+          '49',
+          '53',
+          '54',
+          '46',
+          '48'
+        ]
       }
+    },
+    ready () {
+      this.drawProductTrends()
     },
 
     methods: {
+      drawProductTrends () {
+          // 趋势图表
+        var trendOptions = {
+          noDataLoadingOption: {
+            text: this.$t('common.no_data'),
+            effect: '',
+            effectOption: {
+              backgroundColor: '#FFF'
+            },
+            textStyle: {
+              fontSize: 14,
+              color: '#999'
+            }
+          },
+          calculable: true,
+          // 鼠标移动显示提示
+          tooltip: {
+            trigger: 'axis'
+          },
+          // 图表边距
+          grid: {
+            x: 50,
+            y: 32,
+            x2: 15
+          },
+          // 图表上方显示线条对应数据
+          legend: {
+            x: 'right',
+            y: 10,
+            data: ['水箱温度', '环境温度']
+          },
+          // X轴
+          xAxis: [{
+            type: 'category',
+            boundaryGap: false,
+            data: this.dates
+          }],
+          // Y轴
+          yAxis: [{
+            type: 'value'
+          }],
+          // 每条数据详情
+          series: [{
+            name: '水箱温度',
+            type: 'line',
+            data: this.waterboxTems
+          }, {
+            name: '环境温度',
+            type: 'line',
+            data: this.environmentTems
+          }]
+        }
+        var trendChart = echarts.init(document.getElementById('trendChart'))
+        trendChart.setOption(trendOptions)
+        window.onresize = trendChart.resize
+      }
     }
   }
 </script>
 
 <style lang="stylus">
   @import '../../../../assets/stylus/common'
+  .chartwrap
+    padding 20px 0
 </style>
--- src/views/product/device/params/fault-records.vue
+++ src/views/product/device/params/fault-records.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="panel">
     <div class="panel-bd">
-      故障记录
+      <table class="table table-stripe table-bordered wrongcodetable">
+        <thead>
+          <tr>
+            <th>故障码</th>
+            <th>故障</th>
+            <th>时间</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="record in records| limitBy pageCount (currentPage-1)*pageCount">
+            <td>{{ record.code }}</td>
+            <td>{{ record.fault }}</td>
+            <td>{{ record.time }}</td>
+            <td class="tac">
+              <button @click="editRecord(record)" class="btn btn-link btn-sm">{{ $t("common.del") }}</button>
+            </td>
+          </tr>
+          <tr v-if="records.length === 0">
+            <td colspan="4" class="tac"><i v-if="$loadingRouteData" class="fa fa-refresh fa-spin"></i>
+              <div v-else="v-else" class="tips-null">{{ $t("common.no_records") }}</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <pager v-if="!loadingRecord && records.length > pageCount" :total="records.length" :current.sync="currentPage" :page-count="pageCount"></pager>
     </div>
   </div>
 </template>
 
 <script>
   import { globalMixins } from '../../../../mixins'
+  import Pager from '../../../../components/Pager'
 
   export default {
     name: 'BasicInfo',
@@ -16,16 +42,85 @@
 
     mixins: [globalMixins],
 
+    components: {
+      'pager': Pager
+    },
+
     data () {
       return {
+        records: [
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故障',
+            time: '2016-3-30 13:40'
+          },
+          {
+            code: 123456,
+            fault: '故',
+            time: '2016-3-30 13:40'
+          }
+        ],
+        loadingRecord: false,
+        currentPage: 1,
+        pageCount: 10
       }
     },
 
     methods: {
+      editRecord (params) {
+        console.log(params)
+      }
     }
   }
 </script>
 
 <style lang="stylus">
   @import '../../../../assets/stylus/common'
+  .wrongcodetable,.wrongcodetable th
+    text-align center
 </style>
