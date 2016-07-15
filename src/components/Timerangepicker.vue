4c0df7cbfab560a4ef1222de4fbffec69a72f59b
ShighGoing <shigh945@gmail.com>
2016-07-15 18:30:33 +0800

    拷贝企业管理台的日期选择器和过滤器

---
+++ src/components/Timerangepicker.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="time-range-picker-wrap">
+    <input @click='showChoosePanel=!showChoosePanel' class="time-range-show" readonly="readonly" v-model='timeShowPanel'/>
+    <div v-show='showChoosePanel' class="time-range-picker-panel">
+      <div class="start-time">
+        <span>开始时间:</span>
+        <span class="time">{{startTime | uniformDate}}{{ }}{{startTime | uniformMinuteTime}}</span>
+        <a @click='selectStartTime' class="fa fa-calendar"></a>
+      </div>
+      <div class="end-time">
+        <span>结束时间:</span>
+        <span class="time">{{endTime | uniformDate}}{{ }}{{endTime | uniformMinuteTime}}</span>
+        <a @click='selectEndTime' class="fa fa-calendar"></a>
+      </div>
+      <div class="choose-bar">
+        <table>
+          <tr>
+            <td><a @click='setOneHour'>1小时</a></td>
+            <td><a @click='setSixHour'>6小时</a></td>
+            <td><a @click='setOneDay'>1天</a></td>
+            <td><a @click='setSevenDay'>7天</a></td>
+          </tr>
+        </table>
+        <span>时间间隔最大是15天</span>
+      </div>
+      <div class="choose-submit">
+        <button @click='dispatchTime'>确定</button>
+      </div>
+    </div>
+    <date-time-picker :open.sync='showTimePicker' @timechange='timeChange' :value='defaultTime'></date-time-picker>
+  </div>
+</template>
+
+<script>
+import Datetimepicker from './Datetimepicker'
+import {uniformMinuteTime, uniformDate} from '../filters'
+
+export default {
+  name: 'timerangepicker',
+  components: {
+    'date-time-picker': Datetimepicker
+  },
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      showChoosePanel: false,
+      showTimePicker: false,
+      startTime: '',
+      endTime: '',
+      picker: 0, // 表示starttime在取值， 1表示endtime在取值,
+      defaultTime: new Date()
+    }
+  },
+  computed: {
+    timeShowPanel () {
+      return uniformDate(this.startTime) + ' ' + uniformMinuteTime(this.startTime) + ' - ' + uniformDate(this.endTime) + ' ' + uniformMinuteTime(this.endTime)
+    }
+  },
+  ready () {
+    var curTime = new Date()
+    this.startTime = new Date(curTime.getTime() - 3600 * 1000)
+    this.endTime = curTime
+  },
+  methods: {
+    selectStartTime () {
+      this.picker = 0
+      this.defaultTime = this.startTime
+      this.showTimePicker = true
+    },
+    selectEndTime () {
+      this.picker = 1
+      this.defaultTime = this.endTime
+      this.showTimePicker = true
+    },
+    timeChange (microtime) {
+      var date = new Date(microtime)
+      if (this.picker === 0) {
+        this.startTime = date
+      } else {
+        this.endTime = date
+      }
+    },
+    setOneHour () {
+      this.endTime = new Date()
+      this.startTime = new Date((this.endTime.getTime() - 3600 * 1000))
+    },
+    setSixHour () {
+      this.endTime = new Date()
+      this.startTime = new Date((this.endTime.getTime() - 3600 * 6 * 1000))
+    },
+    setOneDay () {
+      this.endTime = new Date()
+      this.startTime = new Date((this.endTime.getTime() - 3600 * 24 * 1000))
+    },
+    setSevenDay () {
+      this.endTime = new Date()
+      this.startTime = new Date((this.endTime.getTime() - 3600 * 24 * 7 * 1000))
+    },
+    dispatchTime () {
+      this.showChoosePanel = false
+      this.$dispatch('timechange', this.startTime, this.endTime)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.time-range-picker-wrap
+  width 240px
+  margin 6px 10px
+  padding 0px 10px
+  height 28px
+  float right
+  span
+    cursor pointer
+  .time-range-show
+    line-height 28px
+    text-align center
+    display inline-block
+    vertical-align middle
+    font-size 10px
+    width 240px
+    background-color white
+    border none
+  .time-range-picker-panel
+    height 122px
+    width 280px
+    background-color white
+    clear both
+    z-index 99
+    border 2px solid #DEDEDE
+    position absolute
+    margin-top 10px
+    margin-left -22px
+    .start-time, .end-time
+      margin 10px 10px 10px 10px
+      text-align center
+      .time
+        margin-left 10px
+        /*border 2px solid #87C888*/
+        padding 0 15px
+        display inline-block
+        width 120px
+        font-size 10px
+        border 1px solid #EEEEEE
+        background-color #F7F7F7
+      a
+        margin-left 10px
+      span
+        cursor text
+    .choose-submit
+      button
+        float right
+        margin-top 5px
+        margin-right 10px
+        font-size 10px
+        border none
+        background-color #F7F7F7
+        border 1px solid #EEEEEE
+    .choose-bar
+      span
+        float right
+        font-size 10px
+        padding-right 10px
+        cursor text
+      table
+        float left
+        margin 0 10px
+        font-size 10px
+        tr
+          td
+            border 1px solid #EEEEEE
+            background-color #F7F7F7
+            padding 0 3px
+            cursor pointer
+</style>
--- src/filters.js
+++ src/filters.js
@@ -13,26 +13,6 @@ export function formatDate (date) {
 }
 
 /**
- * 日期格式化
- * @param  {String} value 目标日期字符串
- * @return {String}
- */
-export function uniformDate (date) {
-  if (date && date.length > 0) {
-    var formatDate = new Date(date)
-    var year = formatDate.getFullYear().toString()
-    var month = formatDate.getMonth() + 1 > 9 ? (formatDate.getMonth() + 1).toString : '0' + (formatDate.getMonth() + 1).toString()
-    var dates = formatDate.getDate() > 9 ? formatDate.getDate().toString() : '0' + formatDate.getDate().toString()
-    var hours = formatDate.getHours() > 9 ? formatDate.getHours().toString() : '0' + formatDate.getHours().toString()
-    var minutes = formatDate.getMinutes() > 9 ? formatDate.getMinutes().toString() : '0' + formatDate.getMinutes().toString()
-    var seconds = formatDate.getSeconds() > 9 ? formatDate.getSeconds().toString() : '0' + formatDate.getSeconds().toString()
-    return year + '-' + month + '-' + dates + ' ' + hours + ':' + minutes + ':' + seconds
-  } else {
-    return date
-  }
-}
-
-/**
  * 根据条件过滤选项
  * @param  {Array}  value     过滤前的所有可选项
  * @param  {Array}  selected  已选择的所有项
@@ -57,4 +37,119 @@ export function dropSlected (value, selected, curr, prop) {
     })
     return flag
   })
+}
+
+/**
+ * 日期格式化 yyyy-mm-dd
+ * @param  {String} value 目标日期字符串
+ *         {Boolean} correct 修正偏差
+ * @return {String}
+ */
+export function uniformDate (date, correct) {
+  var formatDate = null
+  if (date instanceof Date) {
+    formatDate = date
+  } else if (date && date.length > 0) {
+    formatDate = new Date(date)
+  } else {
+    return date
+  }
+
+  if (correct instanceof Boolean && correct) {
+    formatDate = new Date(formatDate.getTime() - 3600 * 8 * 1000)
+  }
+
+  var year = formatDate.getFullYear().toString()
+  var month = formatDate.getMonth() + 1 > 9 ? (formatDate.getMonth() + 1).toString() : '0' + (formatDate.getMonth() + 1).toString()
+  var dates = formatDate.getDate() > 9 ? formatDate.getDate().toString() : '0' + formatDate.getDate().toString()
+  return year + '-' + month + '-' + dates
+}
+
+/**
+ * 日期格式化 hh:mm:ss
+ * @param  {String} value 目标日期字符串
+ *         {Boolean} correct 修正偏差
+ * @return {String}
+ */
+export function uniformTime (date, correct) {
+  var formatDate = null
+  if (date instanceof Date) {
+    formatDate = date
+  } else if (date && date.length > 0) {
+    formatDate = new Date(date)
+  } else {
+    return date
+  }
+
+  if (correct instanceof Boolean && correct) {
+    formatDate = new Date(formatDate.getTime() - 3600 * 8 * 1000)
+  }
+
+  var hours = formatDate.getHours() > 9 ? formatDate.getHours().toString() : '0' + formatDate.getHours().toString()
+  var minutes = formatDate.getMinutes() > 9 ? formatDate.getMinutes().toString() : '0' + formatDate.getMinutes().toString()
+  var seconds = formatDate.getSeconds() > 9 ? formatDate.getSeconds().toString() : '0' + formatDate.getSeconds().toString()
+  return hours + ':' + minutes + ':' + seconds
+}
+
+/**
+ * 日期格式化 hh:mm
+ * @param  {String} value 目标日期字符串
+ *         {Boolean} correct 修正偏差
+ * @return {String}
+ */
+export function uniformMinuteTime (date, correct) {
+  var formatDate = null
+  if (date instanceof Date) {
+    formatDate = date
+  } else if (date && date.length > 0) {
+    formatDate = new Date(date)
+  } else {
+    return date
+  }
+
+  if (correct) {
+    formatDate = new Date(formatDate.getTime() - 3600 * 8 * 1000)
+  }
+
+  var hours = formatDate.getHours() > 9 ? formatDate.getHours().toString() : '0' + formatDate.getHours().toString()
+  var minutes = formatDate.getMinutes() > 9 ? formatDate.getMinutes().toString() : '0' + formatDate.getMinutes().toString()
+  return hours + ':' + minutes
+}
+
+/**
+ * 处理浮点数,保留2位小数
+ * @param  {Float} value 浮点数
+ * @return {String}
+ */
+export function toDecimal2 (value) {
+  var f = parseFloat(value)
+  if (isNaN(f)) {
+    return ''
+  }
+  f = Math.round(f * 100) / 100
+  var s = f.toString()
+  var rs = s.indexOf('.')
+  if (rs < 0) {
+    rs = s.length
+    s = s + '.'
+  }
+  for (var i = s.length; i <= rs + 2; i++) {
+    s = s + '0'
+  }
+
+  return s
+}
+
+/**
+ * 百分比表示浮点数,保留2位小数： 例 0.0201 => 2.01%
+ * @param  {Float} value 浮点数
+ * @return {String}
+ */
+export function toPercentDecimal2 (value) {
+  var f = parseFloat(value)
+  if (isNaN(f)) {
+    return ''
+  }
+
+  return toDecimal2(f * 100) + '%'
 }
