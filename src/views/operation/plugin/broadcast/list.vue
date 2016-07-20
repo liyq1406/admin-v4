862afc8c928373327ae9fe75c52e5187a49a4b90
qiuguohui <530784837@qq.com>
2016-07-20 11:10:46 +0800

    消息推送列表表格部分

--- src/views/operation/plugin/broadcast/list.vue
+++ src/views/operation/plugin/broadcast/list.vue
@@ -1,73 +1,101 @@
 <template>
-  <section>
+  <div class="main">
+    <div class="main-title">
+      <h2>消息列表</h2>
+    </div>
     <div class="panel">
-      <div class="panel-hd">
-        <search-box :key.sync="query" :active="searching" :placeholder="$t('ui.overview.addForm.search_condi')" @cancel="getHistory(true)" @search-activate="toggleSearching" @search-deactivate="toggleSearching" @search="handleSearch" @press-enter="getHistory(true)">
-          <v-select width="100px" :label="queryType.label">
-            <select v-model="queryType">
-              <option v-for="option in queryTypeOptions" :value="option">{{ option.label }}</option>
-            </select>
-          </v-select>
-          <button slot="search-button" @click="getHistory(true)" class="btn btn-primary"><i class="fa fa-search"></i></button>
-        </search-box>
-        <h2>推送历史列表</h2>
-      </div>
       <div class="panel-bd">
         <div class="data-table with-loading">
-          <div class="icon-loading" v-show="loadingData">
-            <i class="fa fa-refresh fa-spin"></i>
+          <div class="filter-bar">
+            <div class="filter-group fl">
+              <div class="filter-group-item">
+                <v-select label="全部" width='110px' size="small">
+                  <span slot="label">显示</span>
+                </v-select>
+              </div>
+            </div>
+            <div class="filter-group fr">
+              <div class="filter-group-item">
+                <div class="fr">
+                  <a class="fa fa- fa-share-square-o"></a>
+                </div>
+              </div>
+              <div class="filter-group-item">
+                <search-box :key.sync="query" :active="searching" @cancel="getHistories" :placeholder="$t('ui.user.fields.account')" @search-activate="toggleSearching" @search-deactivate="toggleSearching" @search="handleSearch" @press-enter="getHistories">
+                  <button slot="search-button" @click="getHistories" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                  <label>{{ $t('ui.user.search_user') }}</label>
+                </search-box>
+              </div>
+            </div>
           </div>
-          <table class="table table-stripe table-bordered">
-            <thead>
-              <tr>
-                <th>推送内容</th>
-                <th>推送渠道</th>
-                <th>推送方式</th>
-                <th>推送状态</th>
-                <th>推送时间</th>
-              </tr>
-            </thead>
-            <tbody>
-              <template v-if="histories.length > 0">
-                <tr v-for="history in histories">
-                  <!-- <td><a v-link="{path: '/plugins/broadcast/history/' + ($index + 1)}">{{ history.content }}</a></td> -->
-                  <td>{{ history.content }}</td>
-                  <td>{{ history.channel }}</td>
-                  <td>{{ history.type }}</td>
-                  <td>
-                    <span v-if="history.status===1" class="hl-green">已发送</span>
-                    <span v-if="history.status===0" class="hl-gray">待发送
-                      <!-- <a v-link="{path: '/plugins/broadcast/history/' + ($index + 1) + '/update'}" class="fa fa-edit"></a> -->
-                    </span>
-                    <div class="broadcast-details" v-if="history.status===1">
-                      <p>发送：38492</p>
-                      <p>接收：122232</p>
-                    </div>
-                  </td>
-                  <td>{{ history.broadcast_time }}</td>
-                  <!-- <td><a v-link="'/products/' + $route.params.id + '/devices/' + device.id" class="hl-red">{{ device.mac }}</a></td>
-                  <td>{{ device.id }}</td>
-                  <td v-text="device.is_active ? $t('ui.device_list.active') : $t('ui.device_list.not_active')"></td>
-                  <td><span v-if="device.active_date">{{ device.active_date | formatDate }}</span></td>
-                  <td><span v-if="device.is_online" class="hl-green">{{ $t('ui.device_list.online') }}</span><span v-else class="hl-gray">{{ $t('ui.device_list.offline') }}</span></td> -->
-                </tr>
-              </template>
-              <tr v-if="histories.length === 0 && !loadingData">
-                <td colspan="5" class="tac">
-                  <div class="tips-null"><i class="fa fa-exclamation-circle"></i> <span>{{ $t("common.no_records") }}</span></div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <c-table :headers="headers" :tables="tables"></c-table>
         </div>
       </div>
     </div>
-  </section>
+    <!-- <section>
+      <div class="panel">
+        <div class="panel-hd">
+          <search-box :key.sync="query" :active="searching" :placeholder="$t('ui.overview.addForm.search_condi')" @cancel="getHistory(true)" @search-activate="toggleSearching" @search-deactivate="toggleSearching" @search="handleSearch" @press-enter="getHistory(true)">
+            <v-select width="100px" :label="queryType.label">
+              <select v-model="queryType">
+                <option v-for="option in queryTypeOptions" :value="option">{{ option.label }}</option>
+              </select>
+            </v-select>
+            <button slot="search-button" @click="getHistory(true)" class="btn btn-primary"><i class="fa fa-search"></i></button>
+          </search-box>
+          <h2>推送历史列表</h2>
+        </div>
+        <div class="panel-bd">
+          <div class="data-table with-loading">
+            <div class="icon-loading" v-show="loadingData">
+              <i class="fa fa-refresh fa-spin"></i>
+            </div>
+            <table class="table table-stripe table-bordered">
+              <thead>
+                <tr>
+                  <th>推送内容</th>
+                  <th>推送渠道</th>
+                  <th>推送方式</th>
+                  <th>推送状态</th>
+                  <th>推送时间</th>
+                </tr>
+              </thead>
+              <tbody>
+                <template v-if="histories.length > 0">
+                  <tr v-for="history in histories">
+                    <td>{{ history.content }}</td>
+                    <td>{{ history.channel }}</td>
+                    <td>{{ history.type }}</td>
+                    <td>
+                      <span v-if="history.status===1" class="hl-green">已发送</span>
+                      <span v-if="history.status===0" class="hl-gray">待发送
+                      </span>
+                      <div class="broadcast-details" v-if="history.status===1">
+                        <p>发送：38492</p>
+                        <p>接收：122232</p>
+                      </div>
+                    </td>
+                    <td>{{ history.broadcast_time }}</td>
+                  </tr>
+                </template>
+                <tr v-if="histories.length === 0 && !loadingData">
+                  <td colspan="5" class="tac">
+                    <div class="tips-null"><i class="fa fa-exclamation-circle"></i> <span>{{ $t("common.no_records") }}</span></div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section> -->
+  </div>
 </template>
 
 <script>
   import * as config from 'consts/config'
   import Select from 'components/Select'
+  import Table from 'components/Table'
   import SearchBox from 'components/SearchBox'
   import { globalMixins } from 'src/mixins'
 
@@ -78,6 +106,7 @@
 
     components: {
       'v-select': Select,
+      'c-table': Table,
       'search-box': SearchBox
     },
 
@@ -97,11 +126,72 @@
         queryType: {
           label: '推送内容',
           value: 'content'
-        }
+        },
+        headers: [
+          {
+            key: 'title',
+            title: '推送标题',
+            class: 'tac'
+          },
+          {
+            key: 'channel',
+            title: '推送平台',
+            class: 'tac'
+          },
+          {
+            key: 'broadcast_time',
+            title: '推送时间',
+            sortType: -1,
+            class: 'tac'
+          },
+          {
+            key: 'status',
+            title: '推送状态',
+            class: 'tac'
+          },
+          {
+            key: 'valid_count',
+            title: '有效推送量',
+            class: 'tac'
+          },
+          {
+            key: 'arrive_count',
+            title: '抵达数',
+            class: 'tac'
+          },
+          {
+            key: 'arrive_rate',
+            title: '抵达率',
+            class: 'tac'
+          },
+          {
+            key: 'click_count',
+            title: '点击数',
+            class: 'tac'
+          },
+          {
+            key: 'click_rate',
+            title: '点击数',
+            class: 'tac'
+          }
+        ]
       }
     },
 
     computed: {
+      tables () {
+        var result = []
+        this.histories.map((item) => {
+          var history = {
+            title: '<a class="hl-red">' + item.content + '</a>',
+            channel: item.channel,
+            broadcast_time: item.broadcast_time,
+            status: item.status
+          }
+          result.push(history)
+        })
+        return result
+      },
       queryCondition () {
         var condition = {
           filter: ['id', 'mac', 'is_active', 'active_date', 'is_online', 'last_login'],
