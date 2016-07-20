4836a00ef3bef329d06b77cbbf18d9e799b1333f
qiuguohui <530784837@qq.com>
2016-07-20 09:40:56 +0800

    完成新建推送静态页面

--- src/views/operation/plugin/broadcast/add.vue
+++ src/views/operation/plugin/broadcast/add.vue
@@ -1,147 +1,178 @@
 <template>
-  <form v-form name="validation" @submit.prevent="onSubmit">
-    <div class="panel">
-      <div class="panel-bd">
-        <div class="form">
-          <div class="form-row row">
-            <label class="form-control col-5">推送渠道:</label>
-            <div class="controls col-19">
-              <v-select :label="channel.label" width="200px">
-                <select v-model="channel" v-form-ctrl name="channel">
-                  <option v-for="opt in channelOptions" :value="opt">{{ opt.label }}</option>
-                </select>
-              </v-select>
+  <div class="main">
+    <div class="main-title">
+      <h2>新建推送</h2>
+    </div>
+    <div class="container row">
+      <div class="form-box col-16">
+        <form v-form name="validation" @submit.prevent="onSubmit">
+          <div class="panel">
+            <div class="panel-hd">
+              <h3>1 推送人群</h3>
             </div>
-          </div>
-          <div class="form-row row">
-            <label class="form-control col-5">推送类型:</label>
-            <div class="controls col-19">
-              <div class="radio-group">
-                <label v-for="type in types" class="radio">
-                  <input type="radio" v-model="model.type" name="type" :value="$index+1" number/>{{ type }}
-                </label>
-              </div>
-              <div v-show="model.type===3" class="input-text-wrap mt5">
-                <textarea v-model="model.userList" name="userList" class="input-text"></textarea>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row" v-show="model.type===2">
-            <label class="form-control col-5">推送范围:</label>
-            <div class="controls col-19">
-              <div class="row mb10">
-                <div class="col-4 control-text">用户类型</div>
-                <div class="col-20 select-group">
-                  <v-select :label="userType.label" width="100px">
-                    <select v-model="userType" v-form-ctrl name="userType">
-                      <option v-for="opt in userTypeOptions" :value="opt">{{ opt.label }}</option>
-                    </select>
-                  </v-select>
-                  <v-select :label="userTypeRange.label" width="140px" class="ml5">
-                    <select v-model="userTypeRange" v-form-ctrl name="userTypeRange">
-                      <option v-for="opt in userTypeRangeOptions" :value="opt">{{ opt.label }}</option>
-                    </select>
-                  </v-select>
+            <div class="panel-bd">
+              <div class="form">
+                <div class="form-row row">
+                  <label class="form-control col-5">目标平台:</label>
+                  <div class="controls col-19">
+                    <div class="radio-group">
+                      <label class="radio">
+                        <input type="radio">Android
+                      </label>
+                      <label class="radio">
+                        <input type="radio">IOS
+                      </label>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div class="row mb10">
-                <div class="col-4 control-text">地域</div>
-                <div class="col-20">
-                  <area-select :province.sync="selectedProvince" :city.sync="selectedCity" :district.sync="selectedDistrict"></area-select>
+                <div class="form-row row">
+                  <label class="form-control col-5">推送类型:</label>
+                  <div class="controls col-19">
+                    <div class="radio-group">
+                      <label v-for="type in types" class="radio">
+                        <input type="radio" v-model="model.type" name="type" :value="$index+1" number/>{{ type }}
+                      </label>
+                    </div>
+                    <div v-show="model.type===3" class="input-text-wrap mt5">
+                      <textarea v-model="model.userList" name="userList" class="input-text"></textarea>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-4 control-text">标签</div>
-                <div class="col-20">
-                  <div class="checkbox-group">
-                    <label v-for="type in tags" class="checkbox">
-                      <input type="checkbox" v-model="model.tags" name="tags" :value="$index+1" number/>{{ type }}
-                    </label>
+                <div class="form-row row" v-show="model.type===2">
+                  <label class="form-control col-5">推送范围:</label>
+                  <div class="controls col-19">
+                    <div class="row mb10">
+                      <div class="col-4 control-text">用户类型</div>
+                      <div class="col-20 select-group">
+                        <v-select :label="userType.label" width="100px">
+                          <select v-model="userType" v-form-ctrl name="userType">
+                            <option v-for="opt in userTypeOptions" :value="opt">{{ opt.label }}</option>
+                          </select>
+                        </v-select>
+                        <v-select :label="userTypeRange.label" width="140px" class="ml5">
+                          <select v-model="userTypeRange" v-form-ctrl name="userTypeRange">
+                            <option v-for="opt in userTypeRangeOptions" :value="opt">{{ opt.label }}</option>
+                          </select>
+                        </v-select>
+                      </div>
+                    </div>
+                    <div class="row mb10">
+                      <div class="col-4 control-text">地域</div>
+                      <div class="col-20">
+                        <area-select :province.sync="selectedProvince" :city.sync="selectedCity" :district.sync="selectedDistrict"></area-select>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-4 control-text">标签</div>
+                      <div class="col-20">
+                        <div class="checkbox-group">
+                          <label v-for="type in tags" class="checkbox">
+                            <input type="checkbox" v-model="model.tags" name="tags" :value="$index+1" number/>{{ type }}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="form-row row">
-            <label class="form-control col-5">推送时间:</label>
-            <div class="controls col-19">
-              <div class="radio-group">
-                <label v-for="type in startTypes" class="radio">
-                  <input type="radio" v-model="model.startType" name="startType" :value="$index+1" number/>{{ type }}
-                </label>
-              </div>
-              <div class="row mt10" v-show="model.startType>1">
-                <date-picker :value.sync="startDate"></date-picker>
-                <time-picker :value.sync="startTime"></time-picker>
+          <div class="panel">
+            <div class="panel-hd">
+              <h3>2 推送内容</h3>
+            </div>
+            <div class="panel-bd">
+              <div class="form">
+                <div class="row mb10">
+                  <div class="col-19 col-offset-5 hl-gray">iOS平台填写标题即可</div>
+                </div>
+                <div class="form-row row">
+                  <label class="form-control col-5">推送标题:</label>
+                  <div class="controls col-19">
+                    <div v-placeholder="'不可超过20个字符'" class="input-text-wrap">
+                      <input v-model="model.title" type="text" v-form-ctrl name="title" maxlength="20" required custom-validator="noSpacesPrefixAndSuffix" lazy class="input-text"/>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-row row">
+                  <label class="form-control col-5">推送描述:</label>
+                  <div class="controls col-19">
+                    <div v-placeholder="'不可超过200个字符'" class="input-text-wrap">
+                      <input v-model="model.desc" type="text" v-form-ctrl name="desc" maxlength="30" required custom-validator="noSpacesPrefixAndSuffix" lazy class="input-text"/>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="form-row row">
-            <label class="form-control col-5">过期时间:</label>
-            <div class="controls col-19">
-              <date-picker :value.sync="endDate"></date-picker>
-              <time-picker :value.sync="endTime"></time-picker>
+          <div class="panel">
+            <div class="panel-hd">
+              <h3>3 推送时间</h3>
+            </div>
+            <div class="panel-bd">
+              <div class="form">
+                <div class="form-row row">
+                  <label class="form-control col-5">推送时间:</label>
+                  <div class="controls col-19 row">
+                    <div class="radio-group col-8">
+                      <label v-for="type in startTypes" class="radio">
+                        <input type="radio" v-model="model.startType" name="startType" :value="$index+1" number/>{{ type }}
+                      </label>
+                    </div>
+                    <div class="row col-14" v-show="model.startType>1">
+                      <date-picker></date-picker>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+          <div class="panel">
+            <div class="panel-hd">
+              <h3>4 打开方式</h3>
+            </div>
+            <div class="panel-bd">
+              <div class="form">
+                <div class="form-row row">
+                  <label class="form-control col-5">打开消息:</label>
+                  <div class="controls col-19">
+                    <div class="radio-group mb10">
+                      <label v-for="type in openTypes" class="radio">
+                        <input type="radio" v-model="openType" name="openType" :value="$index+1" number/>{{ type }}
+                      </label>
+                    </div>
+                    <div v-show="openType===1" class="input-text-wrap">
+                      <input v-model="model.openContent" type="text" name="openContent" class="input-text"/>
+                    </div>
+                    <div v-show="openType===2" class="input-text-wrap">
+                      <input v-model="model.openContent" type="text" name="openContent" class="input-text"/>
+                    </div>
+                    <div v-show="openType===3" class="input-text-wrap">
+                      <editor :value="model.openContent"></editor>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-actions row">
+                  <div class="col-offset-5">
+                    <button type="submit" class="btn btn-primary btn-lg">{{ $t("common.ok") }}</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="preview-box col-8">
+        预览
       </div>
     </div>
-    <div class="panel">
-      <div class="panel-bd">
-        <div class="form">
-          <div class="row mb10">
-            <div class="col-19 col-offset-5 hl-gray">iOS平台填写标题即可</div>
-          </div>
-          <div class="form-row row">
-            <label class="form-control col-5">推送标题:</label>
-            <div class="controls col-19">
-              <div v-placeholder="'不可超过20个字符'" class="input-text-wrap">
-                <input v-model="model.title" type="text" v-form-ctrl name="title" maxlength="20" required custom-validator="noSpacesPrefixAndSuffix" lazy class="input-text"/>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <label class="form-control col-5">推送描述:</label>
-            <div class="controls col-19">
-              <div v-placeholder="'不可超过200个字符'" class="input-text-wrap">
-                <input v-model="model.desc" type="text" v-form-ctrl name="desc" maxlength="30" required custom-validator="noSpacesPrefixAndSuffix" lazy class="input-text"/>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <label class="form-control col-5">打开消息:</label>
-            <div class="controls col-19">
-              <div class="radio-group mb10">
-                <label v-for="type in openTypes" class="radio">
-                  <input type="radio" v-model="openType" name="openType" :value="$index+1" number/>{{ type }}
-                </label>
-              </div>
-              <div v-show="openType===1" class="input-text-wrap">
-                <input v-model="model.openContent" type="text" name="openContent" class="input-text"/>
-              </div>
-              <div v-show="openType===2" class="input-text-wrap">
-                <input v-model="model.openContent" type="text" name="openContent" class="input-text"/>
-              </div>
-              <div v-show="openType===3" class="input-text-wrap">
-                <editor :value="model.openContent"></editor>
-              </div>
-            </div>
-          </div>
-          <div class="form-actions row">
-            <div class="col-offset-5">
-              <button type="submit" class="btn btn-primary btn-lg">{{ $t("common.ok") }}</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </form>
+  </div>
 </template>
 
 <script>
   import { globalMixins } from 'src/mixins'
   import Select from 'components/Select'
-  import DatePicker from 'components/DatePicker'
+  import DatePicker from 'components/DateTimeSinglePicker'
   import TimePicker from 'components/TimePicker'
   import AreaSelect from 'components/AreaSelect'
   import Editor from 'components/Editor'
@@ -231,7 +262,8 @@
 
   .form
     max-width 640px
-
+  .panel
+    border-bottom 0
   .select-group
     .v-select
       display inline-block
