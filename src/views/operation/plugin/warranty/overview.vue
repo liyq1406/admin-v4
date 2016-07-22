c432735b6661bbd71139220f5a762d2d49dc3c6a
FulukawaA <442873864@qq.com>
2016-07-22 11:45:12 +0800

    在线维保-概览

--- src/views/operation/plugin/warranty/overview.vue
+++ src/views/operation/plugin/warranty/overview.vue
@@ -3,72 +3,232 @@
     <div class="main-title">
       <h2>概览</h2>
     </div>
-    <div class="panel">
-      <div class="panel-bd">
-        概览内容
+    <div class="filter-bar filter-bar-head">
+      <div class="filter-group fl">
+        <div class="filter-group-item">
+          <v-select label="空气净化器" width="110px" size="small">
+            <span slot="label">产品</span>
+          </v-select>
+        </div>
+      </div>
+      <div class="filter-group fr">
+        <div class="filter-group-item">
+          <button class="btn btn-ghost btn-sm"><i class="fa fa-share-square-o"></i></button>
+        </div>
+        <!-- <div class="filter-group-item">
+          <date-time-range-picker></date-time-range-picker>
+        </div> -->
+        <div class="filter-group-item">
+          <radio-button-group :items="locales.data.PERIODS" :value.sync="period"><span slot="label" class="label">{{ $t("common.recent") }}</span></radio-button-group>
+        </div>
       </div>
     </div>
-    <div class="row statistic-group">
-      <div class="col-6">
-        <statistic :info="overviewSummary.respond" :title="overviewSummary.respond.title" align="left"></statistic>
+    <div class="panel">
+      <div class="panel-hd panel-hd-full">
+        <h2>趋势</h2>
       </div>
-      <div class="col-6">
-        <statistic :info="overviewSummary.time" :title="overviewSummary.time.title" align="left"></statistic>
+      <div class="panel-bd">
+        <time-line :data="trends.data" :type="'smooth'"></time-line>
       </div>
-      <div class="col-6">
-        <statistic :info="overviewSummary.year" :title="overviewSummary.year.title" align="left"></statistic>
+    </div>
+    <div class="statistic-group">
+      <div style="display:inline-block;width:19.5%;border-right: 1px solid #d9d9d9">
+        <statistic :info="summary.repairPercent" :title="summary.repairPercent.title" :showchange="true" align="left"></statistic>
       </div>
-      <div class="col-6">
-        <statistic :info="overviewSummary.percent" :title="overviewSummary.percent.title" align="left"></statistic>
+      <div style="display:inline-block;width:19.5%;border-right: 1px solid #d9d9d9">
+        <statistic :info="summary.time" :title="summary.time.title" :showchange="true" align="left"></statistic>
+      </div>
+      <div style="display:inline-block;width:19.5%;border-right: 1px solid #d9d9d9">
+        <statistic :info="summary.maintain" :title="summary.maintain.title" align="left"></statistic>
+      </div>
+      <div style="display:inline-block;width:19.5%;border-right: 1px solid #d9d9d9">
+        <statistic :info="summary.avgYear" :title="summary.avgYear.title" align="left"></statistic>
+      </div>
+      <div style="display:inline-block;width:19.5%">
+        <statistic :info="summary.percent" :title="summary.percent.title" :showchange="true" align="left"></statistic>
+      </div>
+    </div>
+    <div class="panel">
+      <div class="panel-hd panel-hd-full">
+        <h2>维修等级与原因</h2>
+      </div>
+      <div class="panel-bd">
+        <div class="row">
+          <div class="col-7">
+            <pie :data="feedbacks" :height="400"></pie>
+          </div>
+          <div class="col-16 col-offset-1 data-table-wrap" style="min-height: 400px">
+            <div class="data-table">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>标题</th>
+                    <th>未处理</th>
+                    <th>已处理</th>
+                    <th>全部</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>使用帮助</td>
+                    <td>128 (25%)</td>
+                    <td>9283 (75%)</td>
+                    <td>12800</td>
+                  </tr>
+                  <tr>
+                    <td>产品咨询</td>
+                    <td>128 (25%)</td>
+                    <td>9283 (75%)</td>
+                    <td>12800</td>
+                  </tr>
+                  <tr>
+                    <td>投诉建议</td>
+                    <td>128 (25%)</td>
+                    <td>9283 (75%)</td>
+                    <td>12800</td>
+                  </tr>
+                  <tr>
+                    <td>产品故障</td>
+                    <td>128 (25%)</td>
+                    <td>9283 (75%)</td>
+                    <td>12800</td>
+                  </tr>
+                  <tr>
+                    <td>退货申请</td>
+                    <td>128 (25%)</td>
+                    <td>9283 (75%)</td>
+                    <td>12800</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import { globalMixins } from 'src/mixins'
-  import Statistic from 'components/Statistic'
+// import Vue from 'vue'
+// import locales from 'consts/locales/index'
+// import api from 'api'
+import Modal from 'components/Modal'
+import Select from 'components/Select'
+import RadioButtonGroup from 'components/RadioButtonGroup'
+import DateTimeRangePicker from 'components/DateTimeRangePicker'
+import Statistic from 'components/Statistic'
+import TimeLine from 'components/g2-charts/TimeLine'
+import Pie from 'components/g2-charts/Pie'
+// import _ from 'lodash'
+import { globalMixins } from 'src/mixins'
+import { pluginMixins } from '../mixins'
 
-  export default {
-    name: 'Overview',
+import Mock from 'mockjs'
 
-    mixins: [globalMixins],
+export default {
+  name: 'Overview',
 
-    components: {
-      Statistic
-    },
+  mixins: [globalMixins, pluginMixins],
 
-    data () {
-      return {
-        overviewSummary: {
-          respond: {
-            total: 23,
-            change: 2,
-            unit: '分',
-            title: '维修平均响应时间'
-          },
-          time: {
-            total: 2.5,
-            change: -2,
-            unit: 'h',
-            title: '维修平均时长'
-          },
-          year: {
-            total: 2,
-            change: 0,
-            unit: '年',
-            title: '延保平均年限'
-          },
-          percent: {
-            total: 34,
-            change: 12,
-            unit: '%',
-            title: '延保率'
-          }
+  components: {
+    Modal,
+    Pie,
+    'v-select': Select,
+    RadioButtonGroup,
+    DateTimeRangePicker,
+    Statistic,
+    TimeLine
+  },
+
+  data () {
+    return {
+      feedbacks: [
+        {
+          name: '新增维修',
+          value: 50
+        },
+        {
+          name: '新增延保',
+          value: 30
+        },
+        {
+          name: '新增保养',
+          value: 20
         }
-      }
-    },
+      ],
+      summary: {
+        repairPercent: {
+          total: 17,
+          change: 2,
+          unit: '%',
+          title: '维修率'
+        },
+        time: {
+          total: 23,
+          change: 2,
+          unit: '分',
+          title: '维修平均响应时间'
+        },
+        maintain: {
+          total: 95,
+          unit: '%',
+          title: '保养完成率'
+        },
+        avgYear: {
+          total: 2,
+          unit: '年',
+          title: '延保平均年限'
+        },
+        percent: {
+          total: 34,
+          change: '124',
+          unit: '%',
+          title: '延保率'
+        }
+      },
+      // 趋势
+      trends: {
+        data: [],
+        options: {}
+      },
+      // 时间间隔
+      period: 7,
+      // 待选时间间隔
+      // periods: locales.data.PERIODS,
+      // 数据是否加载中
+      loadingData: false
+    }
+  },
 
-    methods: {}
+  ready () {
+    this.trends.data = Mock.mock({
+      'list|16': [{
+        'date|+1': [
+          new Date(2016, 7, 17),
+          new Date(2016, 7, 18),
+          new Date(2016, 7, 19),
+          new Date(2016, 7, 20),
+          new Date(2016, 7, 17),
+          new Date(2016, 7, 18),
+          new Date(2016, 7, 19),
+          new Date(2016, 7, 20),
+          new Date(2016, 7, 17),
+          new Date(2016, 7, 18),
+          new Date(2016, 7, 19),
+          new Date(2016, 7, 20)
+        ],
+        'count|+1': [6, 8, 9, 3, 9, 3, 9, 6, 38, 19, 33, 29],
+        '产品|+1': ['新增维修', '新增维修', '新增维修', '新增维修', '新增延保', '新增延保', '新增延保', '新增延保', '新增保养', '新增保养', '新增保养', '新增保养']
+      }]
+    }).list
+  },
+
+  methods: {
   }
+}
 </script>
+
+<style lang="stylus" scoped>
+@import '../../../../assets/stylus/common'
+</style>
