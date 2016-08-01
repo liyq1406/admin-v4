46a3ac48adf7eb3cff1b55e5efab86d5f132ae71
seishi <185766516@qq.com>
2016-08-01 10:28:28 +0800

    新增 utils 文件，用于存放工具类函数

---
+++ src/helpers/utils.js
@@ -0,0 +1,12 @@
+/**
+ * 返回前 n 天开始与结束时间
+ * @param  {Number} n 天数
+ * @return {Object}
+ *   {
+ *     start: '2015-12-06',
+ *     end: '2015-12-13'
+ *   }
+ */
+export const createDayRange = (n) => {
+  return {}
+}
