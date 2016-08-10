c56106bae8de7bb46e8b6c8d793221db61bc9f04
FulukawaA <442873864@qq.com>
2016-08-10 17:52:32 +0800

    告警-热力分布完成

--- src/views/operation/alert/heat-distribution.vue
+++ src/views/operation/alert/heat-distribution.vue
@@ -4,31 +4,23 @@
       <h2>热力分布</h2>
     </div>
     <!-- TODO -->
-    <div class="mb10 ml15">
+    <!-- <div class="mb10 ml15">
       <a style="color: red">全部</a>
       <i class="arrow">&gt;</i>
       <a style="color: red">国家: 中国 <i class="fa fa-sort-down" style="color: black"></i></a>
       <i class="arrow">&gt;</i>
       <a style="color: red">省份: 广东 <i class="fa fa-sort-down" style="color: black"></i></a>
-    </div>
+    </div> -->
     <div class="filter-bar filter-bar-head">
       <div class="filter-group fl">
         <div class="filter-group-item">
           <v-select :label="currentProduct.name" width="110px" size="small">
             <span slot="label">产品</span>
-            <select v-model="currentProduct" @change="">
+            <select v-model="currentProduct" @change="getRegion">
               <!-- <option :value="currentProduct">{{ currentProduct.name }}</option> -->
               <option v-for="product in products" :value="product">{{ product.name }}</option>
             </select>
           </v-select>
-        </div>
-      </div>
-      <div class="filter-group fr">
-        <div class="filter-group-item">
-          <button class="btn btn-ghost btn-sm"><i class="fa fa-share-square-o"></i></button>
-        </div>
-        <div class="filter-group-item">
-          <radio-button-group :items="periods" :value.sync="period"><span slot="label" class="label">{{ $t("common.recent") }}</span></radio-button-group>
         </div>
       </div>
     </div>
@@ -36,10 +28,10 @@
       <div class="panel-bd">
         <div class="row">
           <div class="col-11">
-            <china-heat-map></china-heat-map>
+            <china-heat-map :data="regionData"></china-heat-map>
           </div>
           <div class="col-12 col-offset-1 data-table-wrap mt20 mb20">
-            <div class="data-table">
+            <div class="data-table" v-if="dataPer.length > 0">
               <table class="table">
                 <thead>
                   <tr>
@@ -49,60 +41,12 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>广东</td>
-                    <td>289</td>
-                    <td>14%</td>
-                  </tr>
-                  <tr>
-                    <td>广西</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>广东</td>
-                    <td>289</td>
-                    <td>14%</td>
-                  </tr>
-                  <tr>
-                    <td>广西</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>湖南</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>湖北</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>江西</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>江苏</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>广东</td>
-                    <td>289</td>
-                    <td>14%</td>
-                  </tr>
-                  <tr>
-                    <td>广西</td>
-                    <td>1769</td>
-                    <td>86%</td>
-                  </tr>
-                  <tr>
-                    <td>湖南</td>
-                    <td>1769</td>
-                    <td>86%</td>
+                  <tr v-for="data in dataPer">
+                    <template v-if="data.value">
+                      <td>{{data.name}}</td>
+                      <td>{{data.value}}</td>
+                      <td>{{data.percent | toPercentDecimal2}}</td>
+                    </template>
                   </tr>
                 </tbody>
               </table>
@@ -112,7 +56,7 @@
       </div>
     </div>
 
-    <div class="panel">
+    <!-- <div class="panel">
       <div class="panel-hd">
         <h3 style="font-weight: normal">级别分布</h3>
       </div>
@@ -168,13 +112,13 @@
         </div>
         <pager v-if="total > countPerPage" :total="total" :current.sync="currentPage" :count-per-page="countPerPage" @page-update="getAlerts"></pager>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 // import _ from 'lodash'
-// import api from 'api'
+import api from 'api'
 import * as config from 'consts/config'
 import Pager from 'components/Pager'
 import RadioButtonGroup from 'components/RadioButtonGroup'
@@ -184,6 +128,7 @@ import SearchBox from 'components/SearchBox'
 import dateFormat from 'date-format'
 import LineChart from 'components/charts/Line'
 import { globalMixins } from 'src/mixins'
+import {numToPercent} from 'helpers/utils'
 
 export default {
   name: 'Alerts',
@@ -208,7 +153,6 @@ export default {
   data () {
     return {
       currentProduct: {},
-      key: '',
       statistics: [],
       total: 0,
       countPerPage: config.COUNT_PER_PAGE,
@@ -216,35 +160,8 @@ export default {
       alertTrends: [],
       today: dateFormat('yyyy-MM-dd', new Date()),
       loadingData: false,
-      period: 7,
-      periods: [
-        {
-          value: 1,
-          label: '24h'
-        },
-        {
-          value: 7,
-          label: '7天'
-        },
-        {
-          value: 30,
-          label: '30天'
-        }
-      ],
-      warningLevel: [
-        {
-          name: '重度',
-          value: 50
-        },
-        {
-          name: '中度',
-          value: 30
-        },
-        {
-          name: '轻度',
-          value: 20
-        }
-      ]
+      dataPer: [],
+      regionData: []
     }
   },
 
@@ -258,6 +175,9 @@ export default {
   watch: {
     products () {
       this.getFirstProduct()
+      // if (this.products.length > 0) {
+      //   this.getRegion()
+      // }
     }
     // period () {
     //   this.getAlertTrends()
@@ -269,6 +189,61 @@ export default {
     // 获取第一个产品@author weijie
     getFirstProduct () {
       this.currentProduct = this.products[0] || {}
+      if (this.products.length > 0) {
+        this.getRegion()
+      }
+    },
+    // 获取区域分布
+    getRegion () {
+      api.alert.getAreaTrend(this.currentProduct.id).then((res) => {
+        if (res.status === 200) {
+          var CNData = res.data['中国']
+          var resData = []
+          var regions = []
+          for (let i in CNData) {
+            if (i !== '通知') {
+              regions.push({
+                name: i,
+                value: CNData[i].通知
+              })
+              for (let j in CNData[i]) {
+                if (j !== '通知') {
+                  let temp = {
+                    province: i,
+                    city: j,
+                    value: CNData[i][j].通知
+                  }
+                  resData.push(temp)
+                }
+              }
+            }
+          }
+          // regions = resData
+          this.sortRegion(regions)
+          this.regionData = resData
+        }
+      }).catch((res) => {
+        this.handleError(res)
+      })
+    },
+
+    sortRegion (regions) {
+      // 由大到小排序
+      regions.sort((a, b) => {
+        if (a.value > b.value) {
+          return -1
+        } else if (a.value < b.value) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+
+      if (regions.length > 10) {
+        this.dataPer = numToPercent(regions.slice(0, 10), 'value')
+      } else {
+        this.dataPer = numToPercent(regions, 'value')
+      }
     }
   }
 }
