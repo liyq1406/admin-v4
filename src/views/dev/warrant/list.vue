ae884491551d59777c918192e6b80f03259c8ccc
FulukawaA <442873864@qq.com>
2016-08-11 18:37:54 +0800

    设备授权-授权记录完成

---
+++ src/views/dev/warrant/list.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="main">
+    <div class="main-title">
+      <h2>授权记录</h2>
+    </div>
+    <div class="panel">
+      <div class="panel-bd">
+        <div class="row statistic-group bt">
+          <div class="col-8">
+            <statistic :info="recordSummary.all" :title="recordSummary.all.title" align="left">
+            </statistic>
+          </div>
+          <div class="col-8">
+            <statistic :info="recordSummary.used" :title="recordSummary.used.title" :has-target="true" align="left">
+            </statistic>
+          </div>
+          <div class="col-8">
+            <statistic :info="recordSummary.rest" :title="recordSummary.rest.title" :has-action="true" align="left" :showchange="true">
+              <div slot="action">
+                <button>
+                购买配额
+                </button>
+              </div>
+            </statistic>
+          </div>
+        </div>
+        <div class="data-table with-loading">
+          <div class="filter-bar">
+            <div class="filter-group fl">
+              <div class="filter-group-item">
+                <span slot="label">明细：</span>
+                <button>手动添加</button>
+                <button>批量导入</button>
+              </div>
+            </div>
+            <div class="filter-group fr">
+              <div class="filter-group-item">
+                <search-box :key.sync="key" :placeholder="$t('ui.overview.addForm.search_condi')" :active="searching" @cancel="" @search-activate="searching=!searching"  @press-enter="">
+                  <v-select width="90px" :label="queryType.label" size="small">
+                    <select v-model="queryType">
+                      <option v-for="option in queryTypeOptions" :value="option">{{ option.label }}</option>
+                    </select>
+                  </v-select>
+                  <button slot="search-button" @click="" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                </search-box>
+              </div>
+            </div>
+          </div>
+          <x-table :headers="headers" :tables="tables" :page="page" :loading="loadingData" @page-count-update="onPageCountUpdate" @current-page-change="onCurrPageChage"></x-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // import Vue from 'vue'
+  // import api from 'api'
+  import * as config from 'consts/config'
+  // import locales from 'consts/locales/index'
+  import Modal from 'components/Modal'
+  import Pager from 'components/Pager'
+  import Select from 'components/Select'
+  // import _ from 'lodash'
+  import { globalMixins } from 'src/mixins'
+  import Table from 'components/Table'
+  import SearchBox from 'components/SearchBox'
+  import Breadcrumb from 'components/Breadcrumb'
+  import Statistic from 'components/Statistic'
+
+  export default {
+    name: 'DataForward',
+
+    mixins: [globalMixins],
+
+    components: {
+      'x-table': Table,
+      'modal': Modal,
+      'pager': Pager,
+      'v-select': Select,
+      Breadcrumb,
+      Statistic,
+      'search-box': SearchBox
+    },
+
+    data () {
+      return {
+        queryTypeOptions: [
+          { label: '添加人', value: 'addman' }
+        ],
+        queryType: { label: '添加人', value: 'addman' },
+        key: '',
+        total: 0,
+        currentPage: 1,
+        countPerPage: config.COUNT_PER_PAGE,
+        recordSummary: {
+          all: {
+            total: 2323,
+            title: '即将到期'
+          },
+          used: {
+            total: 135,
+            title: '待处理数'
+          },
+          rest: {
+            total: 1125,
+            title: '今日延保数'
+          }
+        },
+        headers: [{
+          key: 'time',
+          title: '时间'
+        }, {
+          key: 'warrent',
+          title: '授权数'
+        }, {
+          key: 'style',
+          title: '导入方式'
+        }, {
+          key: 'addman',
+          title: '添加人'
+        }, {
+          key: 'edit',
+          title: '操作'
+        }],
+        alerts: [{}, {}]
+      }
+    },
+
+    route: {
+      data () {
+      }
+    },
+
+    ready () {
+    },
+
+    computed: {
+      tables () {
+        var result = []
+        this.alerts.map((item) => {
+          let alert = {
+            time: '2016-05-17',
+            warrent: 12345,
+            style: '批量导入',
+            addman: 'demo@xlink.com',
+            prototype: item
+          }
+          result.push(alert)
+        })
+        return result
+      }
+    },
+
+    methods: {
+      /**
+       * 当前页码改变
+       * @author weijie
+       * @param  {Number} number 页码
+       */
+      onCurrPageChage (number) {
+        this.currentPage = number
+        this.getAlerts()
+      },
+
+      /**
+       * 每页显示的数量改变
+       * @author weijie
+       * @param  {Number} count 数量
+       */
+      onPageCountUpdate (count) {
+        this.countPerPage = count
+        this.getAlerts(true)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.bt
+  border-top 1px solid #d9d9d9
+</style>
