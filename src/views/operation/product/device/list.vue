da8fd6fae54b096eed296cd63b759b768e18d741
seishi <185766516@qq.com>
2016-08-12 11:25:56 +0800

    产品设备列表在线状态修改

--- src/views/operation/product/device/list.vue
+++ src/views/operation/product/device/list.vue
@@ -278,7 +278,7 @@ export default {
           mac: '<a class="hl-red">' + item.mac + '</a>',
           is_active: item.is_active ? '是' : '否',
           active_date: formatDate(item.active_date),
-          is_online: item.is_online ? '在线' : '下线',
+          is_online: item.is_online ? '<span class="hl-green">在线</span>' : '<span class="hl-gray">下线</span>',
           prototype: item
         }
         result.push(device)
