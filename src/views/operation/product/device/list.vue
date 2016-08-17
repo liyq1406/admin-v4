7c6d1bfd3a2bba69602116e01f292b1ab4bfa0dd
seishi <185766516@qq.com>
2016-08-17 16:26:38 +0800

    运营平台产品信息移除编辑逻辑、设备列表移除添加设备和批量导入逻辑

--- src/views/operation/product/device/list.vue
+++ src/views/operation/product/device/list.vue
@@ -23,21 +23,21 @@
 
     <div class="panel">
       <div class="panel-hd">
-        <div class="actions">
+        <!-- <div class="actions">
           <button @click="showAddModal = true" class="btn btn-success"><i class="fa fa-plus"></i>{{ $t("ui.overview.add_device") }}</button>
           <label :class="{'disabled':importing}" class="btn btn-ghost btn-upload">
             <input type="file" v-el:mac-file="v-el:mac-file" name="macFile" @change.prevent="batchImport"/><i class="fa fa-reply-all"></i>{{ importing ? $t("common.handling") : $t("ui.overview.import_devices") }}
           </label>
-        </div>
+        </div> -->
         <h2>设备列表</h2>
       </div>
       <div class="panel-bd">
         <div class="data-table with-loading">
           <div class="filter-bar">
             <div class="filter-group fr">
-              <div class="filter-group-item">
+              <!-- <div class="filter-group-item">
                 <button class="btn btn-ghost btn-sm"><i class="fa fa-reorder"></i></button>
-              </div>
+              </div> -->
               <div class="filter-group-item">
                 <search-box :key.sync="query" :active="searching" :placeholder="$t('ui.overview.addForm.search_condi')" @cancel="getDevices" @search-activate="toggleSearching" @search-deactivate="toggleSearching" @search="handleSearch" @press-enter="getDevices">
                   <v-select width="90px" :label="queryType.label" size="small">
@@ -59,65 +59,7 @@
             </div>
           </div>
           <c-table :headers="headers" :tables="tables" :page="page" :loading="loadingData" @theader-active-date="sortBy" @theader-is-online="sortBy" @tbody-mac="linkToDetails" @page-count-update="onPageCountUpdate" @current-page-change="onCurrPageChage"></c-table>
-          <!-- <table class="table table-stripe table-bordered">
-            <thead>
-              <tr>
-                <th @click="sortBy('mac')" :class="{active: sortKey === 'mac'}">MAC<i :class="sortOrders['mac'] ==='asc' ? 'fa-caret-up' : 'fa-caret-down'" class="fa"></i></th>
-                <th>{{ $t('ui.device.id') }}</th>
-                <th>{{ $t('ui.device.is_active') }}</th>
-                <th @click="sortBy('active_date')" :class="{active: sortKey === 'active_date'}">{{ $t('ui.device.active_date') }}<i :class="sortOrders['active_date'] ==='asc' ? 'fa-caret-up' : 'fa-caret-down'" class="fa"></i></th>
-                <th>{{ $t('ui.device.is_online') }}</th>
-              </tr>
-            </thead>
-            <tbody>
-              <template v-if="devices.length > 0">
-                <tr v-for="device in devices">
-                  <td><a v-link="$route.path + '/' + device.id" class="hl-red">{{ device.mac }}</a></td>
-                  <td>{{ device.id }}</td>
-                  <td v-text="device.is_active ? $t('ui.device_list.active') : $t('ui.device_list.not_active')"></td>
-                  <td><span v-if="device.active_date">{{ device.active_date | formatDate }}</span></td>
-                  <td><span v-if="device.is_online" class="hl-green">{{ $t('ui.device_list.online') }}</span><span v-else class="hl-gray">{{ $t('ui.device_list.offline') }}</span></td>
-                </tr>
-              </template>
-              <tr v-if="devices.length === 0">
-                <td colspan="5" class="tac">
-                  <div class="tips-null"><i class="fa fa-exclamation-circle"></i> <span>{{ $t("common.no_records") }}</span></div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="row">
-          <div class="col-6 mt10">
-            <button v-link="{path: '/products/' + this.$route.params.id + '/records'}" class="btn btn-ghost btn-sm"><i class="fa fa-list"></i><span class="btn-txt">查看上下线历史记录</span></button>
-          </div>
-          <div class="col-18">
-            <pager v-if="total > countPerPage" :total="total" :current.sync="currentPage" :count-per-page="countPerPage" @page-update="getDevices"></pager>
-          </div>
-        </div> -->
       </div>
-      <!-- 添加设备浮层-->
-      <modal :show.sync="showAddModal">
-        <h3 slot="header">{{ $t("ui.overview.add_device") }}</h3>
-        <div slot="body" class="form">
-          <form v-form name="addValidation" @submit.prevent="onAddSubmit" hook="addFormHook">
-            <div class="form-row row">
-              <label class="form-control col-6">{{ $t("ui.overview.addForm.mac") }}:</label>
-              <div class="controls col-18">
-                <div v-placeholder="$t('ui.overview.addForm.mac_placeholder')" class="input-text-wrap">
-                  <input v-model="addModel.mac" type="text" v-form-ctrl name="mac" required lazy class="input-text"/>
-                </div>
-                <div v-if="addValidation.$submitted && addValidation.mac.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.mac.$error.required">{{ $t('ui.validation.required', {field: $t('ui.overview.addForm.mac')}) }}</span></div>
-                <div v-if="addValidation.mac.$dirty" class="form-tips form-tips-error"><span v-if="addValidation.mac.$error.required">{{ $t('ui.validation.required', {field: $t('ui.overview.addForm.mac')}) }}</span></div>
-              </div>
-            </div>
-            <div class="form-actions">
-              <button @click.prevent.stop="onAddCancel" class="btn btn-default">{{ $t("common.cancel") }}</button>
-              <button type="submit" :disabled="adding" :class="{'disabled':adding}" v-text="adding ? $t('common.handling') : $t('common.ok')" class="btn btn-primary"></button>
-            </div>
-          </form>
-        </div>
-      </modal>
     </div>
   </div>
 </template>
@@ -180,15 +122,7 @@ export default {
       total: 0,
       currentPage: 1,
       countPerPage: config.COUNT_PER_PAGE,
-      showAddModal: false,
-      addModel: {
-        mac: ''
-      },
-      addValidation: {},
-      originAddModel: {},
       // querying: false,
-      adding: false,
-      importing: false,
       loadingData: false,
       queryTypeOptions: [
         { label: 'MAC', value: 'mac' },
@@ -262,6 +196,7 @@ export default {
   },
 
   computed: {
+    // 分页信息
     page () {
       var result = {
         total: this.total,
@@ -270,6 +205,8 @@ export default {
       }
       return result
     },
+
+    // 列表数据
     tables () {
       var result = []
       this.devices.map((item) => {
@@ -399,96 +336,6 @@ export default {
     // 取消搜索
     cancelSearching () {
       this.getDevices()
-    },
-
-    // 添加表单钩子
-    addFormHook (form) {
-      this.addForm = form
-    },
-
-    // 关闭添加浮层并净化添加表单
-    resetAdd () {
-      this.adding = false
-      this.showAddModal = false
-      this.addModel = _.clone(this.originAddModel)
-      this.$nextTick(() => {
-        this.addForm.setPristine()
-      })
-    },
-
-    // 取消添加
-    onAddCancel () {
-      this.resetAdd()
-    },
-
-    // 添加操作
-    onAddSubmit () {
-      if (this.addValidation.$valid && !this.adding) {
-        this.adding = true
-        api.device.add(this.$route.params.id, this.addModel).then((res) => {
-          if (res.status === 200) {
-            this.resetAdd()
-            this.getDevices()
-          }
-        }).catch((res) => {
-          this.handleError(res)
-          this.adding = false
-        })
-      }
-    },
-
-    // 批量导入
-    batchImport () {
-      var file = this.$els.macFile.files[0]
-      if (window.File && window.FileReader && window.FileList && window.Blob) {
-        var reader = new window.FileReader()
-        if (!/text\/\w+/.test(file.type)) {
-          this.showNotice({
-            type: 'error',
-            content: file.name + this.$t('ui.upload.type_tips')
-          })
-          return false
-        }
-        reader.onerror = (evt) => {
-          this.showNotice({
-            type: 'error',
-            content: this.$t('ui.upload.read_err')
-          })
-        }
-        this.importing = true
-        // 读取完成
-        reader.onloadend = (evt) => {
-          if (evt.target.readyState === window.FileReader.DONE) {
-            var macArr = evt.target.result.replace(' ', '').replace(/\r\n/g, '\n').split('\n')
-            var a = []
-            macArr.forEach((element, index) => {
-              if (element !== '') {
-                a.push(element)
-              }
-            })
-            macArr = a
-            api.device.batchImport(this.$route.params.id, macArr).then((res) => {
-              if (res.status === 200) {
-                this.showNotice({
-                  type: 'success',
-                  content: this.$t('ui.upload.success_msg')
-                })
-                this.getDevices()
-              }
-              this.importing = false
-            }).catch((res) => {
-              this.handleError(res)
-              this.importing = false
-            })
-          }
-        }
-        reader.readAsText(file)
-      } else {
-        this.showNotice({
-          type: 'error',
-          content: this.$t('ui.upload.compatiblity')
-        })
-      }
     }
   }
 }
--- src/views/operation/product/overview/index.vue
+++ src/views/operation/product/overview/index.vue
@@ -7,7 +7,7 @@
       <div class="product-card">
         <div class="thumb"><img :src="deviceThumb"/></div>
         <div class="info">
-          <h2>{{ currentProduct.name }} <a href="#" @click.prevent="editProduct" class="fa fa-edit"></a></h2>
+          <h2>{{ currentProduct.name }}</h2>
           <div class="desc">{{ currentProduct.description }}</div>
 
           <!-- Start: 产品信息统计 -->
@@ -41,118 +41,19 @@
     <product-trend></product-trend>
     <product-active></product-active>
     <product-distribution></product-distribution>
-
-    <!-- 编辑产品浮层-->
-    <modal :show.sync="showEditModal">
-      <h3 slot="header">{{ $t("ui.overview.editForm.header") }}</h3>
-      <div slot="body" class="form">
-        <form v-form name="editValidation" @submit.prevent="onEditSubmit" hook="editFormHook">
-          <div class="form-row row">
-            <label class="form-control col-6">{{ $t("ui.product.fields.name") }}:</label>
-            <div class="controls col-18">
-              <div v-placeholder="$t('ui.product.placeholders.name')" class="input-text-wrap">
-                <input v-model="editModel.name" type="text" v-form-ctrl name="name" maxlength="32" required custom-validator="noSpacesPrefixAndSuffix" lazy class="input-text"/>
-              </div>
-              <div v-if="editValidation.$submitted && editValidation.name.$pristine" class="form-tips form-tips-error"><span v-if="editValidation.name.$error.required">{{ $t('ui.validation.required', {field: $t('ui.product.fields.name')}) }}</span></div>
-              <div v-if="editValidation.name.$dirty" class="form-tips form-tips-error"><span v-if="editValidation.name.$error.required">{{ $t('ui.validation.required', {field: $t('ui.product.fields.name')}) }}</span><span v-if="editValidation.name.$error.maxlength">{{ $t('ui.validation.maxlength', [ $t('ui.product.fields.name'), 32]) }}</span><span v-if="editValidation.name.$error.customValidator">{{ $t('ui.validation.format', {field: $t('ui.product.fields.name')}) }}</span></div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <label class="form-control col-6">{{ $t("ui.product.fields.desc") }}:</label>
-            <div class="controls col-18">
-              <div v-placeholder="$t('ui.product.placeholders.desc')" class="input-text-wrap">
-                <textarea v-model="editModel.description" type="text" v-form-ctrl name="description" maxlength="250" required lazy class="input-text"></textarea>
-              </div>
-              <div v-if="editValidation.$submitted && editValidation.description.$pristine" class="form-tips form-tips-error"><span v-if="editValidation.description.$error.required">{{ $t('ui.validation.required', {field: $t('ui.product.fields.desc')}) }}</span></div>
-              <div v-if="editValidation.description.$dirty" class="form-tips form-tips-error"><span v-if="editValidation.description.$error.required">{{ $t('ui.validation.required', {field: $t('ui.product.fields.desc')}) }}</span><span v-if="editValidation.description.$error.maxlength">{{ $t('ui.validation.maxlength', [ $t('ui.product.fields.desc'), 250]) }}</span></div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <label class="form-control col-6">{{ $t("ui.product.fields.link_type") }}:</label>
-            <div class="controls col-18">
-              <div class="select">
-                <v-select :label="deviceTypes[editModel.link_type-1]">
-                  <select v-model="editModel.link_type" v-form-ctrl name="link_type">
-                    <option v-for="type in deviceTypes" :value="$index+1" :selected="$index===0">{{ type }}</option>
-                  </select>
-                </v-select>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <div class="controls col-18 col-offset-6">
-              <div class="checkbox-group">
-                <label class="checkbox">
-                  <input type="checkbox" name="is_registerable" v-model="editModel.is_registerable"/>{{ $t("ui.product.fields.is_registerable") }}
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row" v-show="editModel.link_type===5">
-            <div class="controls col-18 col-offset-6">
-              <div class="checkbox-group">
-                <label class="checkbox">
-                  <input type="checkbox" name="is_active_register" v-model="editModel.is_active_register"/>允许动态注册设备
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <div class="controls col-18 col-offset-6">
-              <div class="checkbox-group">
-                <label class="checkbox">
-                  <input type="checkbox" name="ifsnapshot" v-model="editModel.ifsnapshot"/>开启快照功能
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-row row">
-            <div class="controls col-18 col-offset-6">
-              <div class="checkbox-group">
-                <label class="checkbox">
-                  <input type="checkbox" name="is_allow_multi_admin" v-model="editModel.is_allow_multi_admin"/>允许设备多个管理员
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-actions">
-            <label class="del-check">
-              <input type="checkbox" name="del" v-model="delChecked"/>{{ $t("ui.overview.editForm.del") }}
-            </label>
-            <label class="del-check">
-              <input type="checkbox" name="is_release" v-model="editModel.is_release"/>{{ $t("ui.product.fields.is_release") }}
-            </label>
-            <button @click.prevent.stop="onEditCancel" class="btn btn-default">{{ $t("common.cancel") }}</button>
-            <button type="submit" :disabled="editing" :class="{'disabled':editing}" v-text="editing ? $t('common.handling') : $t('common.ok')" class="btn btn-primary"></button>
-          </div>
-        </form>
-      </div>
-    </modal>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import locales from 'consts/locales/index'
-import api from 'api'
 import store from 'store/index'
 import { removeProduct, updateProduct } from 'store/actions/products'
-import Select from 'components/Select'
-import Modal from 'components/Modal'
 import Panel from 'components/Panel'
-import RadioButtonGroup from 'components/RadioButtonGroup'
 import Statistic from 'components/Statistic2'
 import Tooltip from 'components/Tooltip'
-import TimeLine from 'components/g2-charts/TimeLine'
-import Interval from 'components/g2-charts/Interval'
 import IntervalIcon from 'components/g2-charts/IntervalIcon'
-import Pie from 'components/g2-charts/Pie'
-import ChinaMap from 'components/g2-charts/ChinaMap'
 import defaultDeviceThumb from 'assets/images/device_thumb.png'
 import { globalMixins } from 'src/mixins'
 import { productSummaryMixin, setCurrProductMixin } from '../mixins'
-import _ from 'lodash'
-import { createDayRange, beautify } from 'helpers/utils'
 import ProductTrend from './product-trend'
 import ProductActive from './product-active'
 import ProductDistribution from './product-distribution'
@@ -175,20 +76,13 @@ export default {
   },
 
   components: {
-    Modal,
     Panel,
-    RadioButtonGroup,
     Statistic,
     Tooltip,
-    TimeLine,
-    Interval,
     IntervalIcon,
-    ChinaMap,
-    Pie,
     ProductTrend,
     ProductActive,
-    ProductDistribution,
-    'v-select': Select
+    ProductDistribution
   },
 
   data () {
@@ -198,24 +92,6 @@ export default {
         curr: 0,
         labels: ['激活设备', '累计激活']
       },
-      customMargin: [30, 10, 30, 30],
-      deviceTypes: locales[Vue.config.lang].data.DEVICE_TYPES,
-      delChecked: false,
-      // product: {},
-      showEditModal: false,
-      editModel: {
-        is_allow_multi_admin: false,
-        ifsnapshot: false,
-        name: '',
-        description: '',
-        link_type: '',
-        is_registerable: false,
-        is_active_register: false,
-        is_release: false,
-        id: ''
-      },
-      originEditModel: {},
-      editValidation: {},
       // 统计
       statistic: {
         // 用户总数
@@ -256,227 +132,9 @@ export default {
   route: {
     data () {
       this.getCurrProduct(this.$route.params.id)
-      // this.getActivatedTrends(1)
 
       // getProductSummary 方法来自 productSummaryMixin
       this.getProductSummary()
-    }
-  },
-
-  computed: {
-
-  },
-
-  ready () {
-  },
-
-  methods: {
-    /**
-     * 获取产品激活趋势
-     * @param  {Number} type 类型
-     */
-    getActivatedTrends (type) {
-      this.trendTabs.curr = type - 1
-      let range = createDayRange(0, this.trends.activated.period)
-      api.statistics.getProductTrend(this.$route.params.id, range.start, range.end).then((res) => {
-        if (res.status === 200) {
-          let trendData = beautify(res.data, this.trends.activated.period)
-          this.trends.activated.data = trendData.map((item) => {
-            return {
-              date: item.day,
-              count: item.activated,
-              product: this.currentProduct.name
-            }
-          })
-        }
-      })
-    },
-
-    // 获取当前产品
-    // getProduct () {
-    //   api.product.getProduct(this.$route.params.id).then((res) => {
-    //     if (res.status === 200) {
-    //       this.product = res.data
-    //     }
-    //   }).catch((res) => {
-    //     this.handleError(res)
-    //   })
-    // },
-
-    // 编辑表单钩子
-    editFormHook (form) {
-      this.editForm = form
-    },
-
-    // 关闭编辑浮层并净化编辑表单
-    resetEdit () {
-      this.editing = false
-      this.showEditModal = false
-      this.delChecked = false
-      setTimeout(() => {
-        this.editModel = this.originEditModel
-      }, 1000)
-    },
-
-    // 初始化产品编辑表单
-    editProduct () {
-      api.snapshot.getRule(this.$route.params.id).then((res) => {
-        // console.log(res.data.list[0] && res.data.list[0].rule)
-        this.showEditModal = true
-        // this.editModel = _.clone(this.product)
-        this.editModel.ifsnapshot = false
-        this.editModel.name = this.currentProduct.name
-        this.editModel.description = this.currentProduct.description
-        this.editModel.link_type = this.currentProduct.link_type
-        this.editModel.is_registerable = this.currentProduct.is_registerable
-        this.editModel.is_active_register = this.currentProduct.is_active_register
-        this.editModel.is_release = this.currentProduct.is_release
-        this.editModel.id = this.$route.params.id
-        this.editModel.is_allow_multi_admin = this.currentProduct.is_allow_multi_admin
-        this.originEditModel = _.clone(this.editModel)
-
-        // TODO: 优化逻辑
-        if (res.data.list.length) {
-          if (res.data.list[0].rule === 0) {
-            this.editModel.ifsnapshot = false
-          } else {
-            this.editModel.ifsnapshot = true
-          }
-        } else {
-          this.editModel.ifsnapshot = false
-        }
-      })
-    },
-
-    // 取消编辑
-    onEditCancel () {
-      this.resetEdit()
-      // this.product = this.originEditModel
-    },
-
-    // 提交更新
-    onEditSubmit () {
-      if (this.delChecked && !this.editing) {
-        this.editing = true
-        var result = window.confirm('确认删除该产品吗?')
-        if (result === true) {
-          api.product.deleteProduct(this.$route.params.id).then((res) => {
-            if (res.status === 200) {
-              this.resetEdit()
-              this.removeProduct(this.currentProduct)
-              this.$route.router.go('/dashboard')
-            }
-          }).catch((res) => {
-            this.handleError(res)
-            this.editing = false
-          })
-        } else {
-          this.editing = false
-        }
-      } else if (this.editValidation.$valid && !this.editing) {
-        this.editing = true
-        if (this.editModel.link_type === 5) {
-          api.product.updateProduct(this.editModel).then(() => {
-            api.product.getProduct(this.$route.params.id).then((res) => {
-              if (res.status === 200) {
-                // this.product = res.data
-                this.resetEdit()
-                this.updateProduct(res.data)
-              }
-            })
-          }).catch((res) => {
-            this.handleError(res)
-            this.editing = false
-          })
-        } else {
-          var model = {
-            name: this.editModel.name,
-            description: this.editModel.description,
-            link_type: this.editModel.link_type,
-            is_registerable: this.editModel.is_registerable,
-            is_release: this.editModel.is_release,
-            is_allow_multi_admin: this.editModel.is_allow_multi_admin,
-            id: this.editModel.id
-          }
-          api.product.updateProduct(model).then(() => {
-            api.product.getProduct(this.$route.params.id).then((res) => {
-              if (res.status === 200) {
-                // this.product = res.data
-                this.resetEdit()
-                this.updateProduct(res.data)
-              }
-            })
-          }).catch((res) => {
-            this.handleError(res)
-            this.editing = false
-          })
-        }
-        if (this.editModel.ifsnapshot) {
-          var index = []
-          for (let i = 0; i < 45; i++) {
-            index.push(i)
-          }
-          var params = {
-            rule: 3,
-            interval: 30,
-            storage: {
-              // limit: 0,
-              expire: 86400
-            },
-            datapoint: index
-          }
-          api.snapshot.getRule(this.$route.params.id).then((res) => {
-            if (res.data.list.length === 0) {
-              console.log(params)
-              // console.log(res)
-              api.snapshot.createRule(this.$route.params.id, params).then((res) => {
-              })
-            } else {
-              var index = []
-              for (let i = 0; i < 45; i++) {
-                index.push(i)
-              }
-              var newParams = {
-                _id: res.data.list[0].id,
-                rule: 3,
-                interval: 30,
-                storage: {
-                  // limit: 0,
-                  expire: 86400
-                },
-                datapoint: index
-              }
-              api.snapshot.updateRule(this.$route.params.id, newParams).then((res) => {
-                console.log('Update rule')
-              })
-            }
-          })
-        } else {
-          api.snapshot.getRule(this.$route.params.id).then((res) => {
-            if (res.data.list.length === 0) {
-              console.log(11)
-            } else {
-              var index = []
-              for (let i = 0; i < 45; i++) {
-                index.push(i)
-              }
-              var morNewParams = {
-                _id: res.data.list[0].id,
-                rule: 0,
-                interval: 30,
-                storage: {
-                  // limit: 0,
-                  expire: 86400
-                },
-                datapoint: index
-              }
-              api.snapshot.updateRule(this.$route.params.id, morNewParams).then((res) => {
-                console.log(111)
-              })
-            }
-          })
-        }
-      }
     }
   }
 }
