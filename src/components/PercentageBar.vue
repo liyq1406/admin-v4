c7f651b1ff59f2cdf36d4034c2f507462dfbd6b7
seishi <185766516@qq.com>
2016-08-24 13:41:34 +0800

    添加百分占比条组件

---
+++ src/components/PercentageBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="x-percentage-bar">
+    <div class="label">{{ percentage }}</div>
+    <div class="bar-wrap">
+      <span class="bar" :style="barStyles"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PercentageBar',
+
+  props: {
+    // 颜色
+    color: {
+      type: String,
+      default: '#72D5DD'
+    },
+
+    // 占比
+    percentage: {
+      type: String,
+      default: '0%'
+    }
+  },
+
+  computed: {
+    barStyles () {
+      return {
+        backgroundColor: this.color,
+        width: this.percentage
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.x-percentage-bar
+  clearfix()
+
+  .label
+    float left
+    height 20px
+    font-size 12px
+    width 50px
+    line-height 20px
+
+  .bar-wrap
+    margin-left 50px
+    height 16px
+    line-height 20px
+
+    .bar
+      display inline-block
+      height 20px
+</style>
