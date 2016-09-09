3f3da3eba5c22717881c17aa9cf1d7a4e0877da3
qiuguohui <530784837@qq.com>
2016-09-09 14:39:55 +0800

    完成产品字段的获取和编辑字段名和切换显示功能

--- src/views/dev/product/info/edit/field.vue
+++ src/views/dev/product/info/edit/field.vue
@@ -41,7 +41,7 @@
               <th class="tac">{{$index + 1}}</th>
               <th>{{field.name}}</th>
               <th>
-                <a class="hl-red">{{field.label}}</a>
+                <a class="hl-red" @click="onShowEditLabelModal(field, $index)">{{field.label}}</a>
               </th>
               <th class="tac">{{translate(field.type)}}</th>
               <th class="tac">
@@ -66,20 +66,19 @@
         <validator name="editLabelValidation">
           <form novalidate @submit.prevent="onEditLabelSubmit">
             <div class="form-row row">
-              <label class="form-control col-4">请输入字段名:</label>
-              <div class="controls col-20">
+              <label class="form-control col-5">请输入字段名:</label>
+              <div class="controls col-19">
                 <div v-placeholder="$t('ui.product.placeholders.mode')" class="input-text-wrap">
                   <input v-model="editLabelModal.label" v-validate:label="{required: true, maxlength: 20}" type="text" name="editLabelModal.label" class="input-text"/>
                 </div>
-                <div class="form-tips form-tips-error">
-                  <span v-if="$editLabelValidation.label.touched && $editLabelValidation.label.required">请输入字段名</span>
-                  <span v-if="$editLabelValidation.label.modified && $editLabelValidation.label.maxlength">字段名不能超过20个字符</span>
+                <div class="form-tips" :class="{'form-tips-error': $editLabelValidation.label.touched && $editLabelValidation.label.required || $editLabelValidation.label.modified && $editLabelValidation.label.maxlength}">
+                  <span>注：字段名不能为空，且不超过20个字符</span>
                 </div>
               </div>
             </div>
             <div class="form-actions">
               <button type="submit" :disabled="editing" :class="{'disabled':editing}" v-text="editing ? $t('common.handling') : $t('common.ok')" class="btn btn-primary"></button>
-              <button @click.prevent.stop="showDeviceEditModal = false" class="btn btn-default">{{ $t("common.cancel") }}</button>
+              <button @click.prevent.stop="onCancel" class="btn btn-default">{{ $t("common.cancel") }}</button>
             </div>
           </form>
         </validator>
@@ -94,14 +93,13 @@ import api from 'api'
 import Select from 'components/Select'
 import Switch from 'components/Switch'
 import Modal from 'components/Modal'
-// import _ from 'lodash'
-// import { globalMixins } from 'src/mixins'
+import _ from 'lodash'
+import { globalMixins } from 'src/mixins'
 
 export default {
   name: 'field',
 
-  // setCurrProductMixin 保证每个产品相关的页面都能正确访问到当前的产品信息
-  // mixins: [globalMixins, productSummaryMixin, setCurrProductMixin],
+  mixins: [globalMixins],
 
   components: {
     'x-select': Select,
@@ -111,7 +109,7 @@ export default {
 
   data () {
     return {
-      base_fileds: [
+      base_fields: [
         {
           'name': 'mac',
           'label': 'MAC地址',
@@ -174,7 +172,8 @@ export default {
       showEditLabelModal: false,
 
       editLabelModal: {
-        label: ''
+        label: '',
+        index: 0
       },
       showEditSortModal: false,
 
@@ -212,6 +211,25 @@ export default {
 
   methods: {
     /**
+     * 浮层的取消按钮
+     * @return {[type]} [description]
+     */
+    onCancel () {
+      this.showEditLabelModal = false
+      this.showEditSortModal = false
+      this.$resetValidation()
+    },
+    /**
+     * 显示编辑字段名浮层
+     * @param  {[type]} field [description]
+     * @return {[type]}       [description]
+     */
+    onShowEditLabelModal (field, index) {
+      this.showEditLabelModal = true
+      this.editLabelModal.label = field.label
+      this.editLabelModal.index = index
+    },
+    /**
      * 获取数据端点
      * @return {[type]} [description]
      */
@@ -233,7 +251,9 @@ export default {
      * @return {[type]} [description]
      */
     onEditLabelSubmit () {
-      console.log('编辑字段名表单提交')
+      let fieldList = _.clone(this.fieldList)
+      fieldList[this.editLabelModal.index].label = this.editLabelModal.label
+      this.setFiled(fieldList)
     },
     /**
      * 是否显示按钮切换事件
@@ -242,13 +262,14 @@ export default {
      */
     toggleHidden (field) {
       field.hidden = !field.hidden
-      // console.log('这里要发请求出去设置隐藏与否')
-      // console.log(this.sortDataList())
       this.setFiled()
     },
-    setFiled () {
+    setFiled (fieldList) {
       this.loadingDataField = true
-      api.product.getProductField(this.$route.params.id, this.sortDataList()).then((res) => {
+      console.log('调用设置接口')
+      console.log(this.sortDataList(fieldList))
+      api.product.setProductField(this.$route.params.id, this.sortDataList(fieldList)).then((res) => {
+        this.onCancel()
         this.dataList = res.data
         this.loadingDataField = false
       }).catch((res) => {
@@ -263,7 +284,7 @@ export default {
     sortDataList (fieldList) {
       fieldList = fieldList || this.fieldList
       let result = {}
-      result.base_fileds = []
+      result.base_fields = []
       result.datapoints = []
       fieldList.forEach((item, index) => {
         let obj = {
@@ -271,6 +292,10 @@ export default {
           'label': item.label,
           'hidden': item.hidden,
           'sort': index + 1
+        }
+        if (item.type === 'datapoints') {
+          console.log(item.index)
+          obj.index = item.index
         }
         result[item.type].push(obj)
       })
@@ -289,88 +314,91 @@ export default {
         this.loadingDataField = false
         this.handleError(res)
       })
-      // setTimeout(() => {
-      //   this.dataList = {
-      //     'base_fileds': [
-      //       {
-      //         'name': '111',
-      //         'label': '222',
-      //         'hidden': true,
-      //         'sort': 1
-      //       },
-      //       {
-      //         'name': '111',
-      //         'label': '222',
-      //         'hidden': false,
-      //         'sort': 2
-      //       }
-      //     ],
-      //     'datapoints': [
-      //       {
-      //         'index': 0,
-      //         'name': '222',
-      //         'label': '333',
-      //         'hidden': true,
-      //         'sort': 0
-      //       },
-      //       {
-      //         'index': 0,
-      //         'name': '222',
-      //         'label': '333',
-      //         'hidden': false,
-      //         'sort': 3
-      //       }
-      //     ]
-      //   }
-      //   this.loading = false
-      // }, 1000)
     },
     /**
      * 初始化 将服务器返回的数据合并成一个数据用于渲染页面
      * @return {[type]} [description]
      */
     init () {
+      // 判断数据是否加载完成 如果正在loading则return
       if (this.loading) {
         return
       }
+      // 存放最终的计算结果
       var result = []
+      // 是否需要向后端重置数据的标志位 弱下方执行完成之后标志位为true 就会去向服务器发请求重置当前的字段列表
       var needResetData = false
-      if (!this.dataList.base_fileds) {
+      // 判断服务器时候有返回基本字段信息
+      if (!this.dataList.base_fields) {
+        // 若没有返回基本字段信息 则将标志位设置为true
         needResetData = true
-        this.dataList.base_fileds = this.base_fileds
+        // 读取默认的基本字段信息 默认前五个显示 三个隐藏 一共八个 具体看data()里面的定义
+        this.dataList.base_fields = this.base_fields
       }
-      this.dataList.base_fileds.forEach((item) => {
+      // 初始化基本字段信息 给基本字段加上是否显示字段 加上类型字段
+      this.dataList.base_fields.forEach((item) => {
         item.show = !item.hidden
-        item.type = 'base_fileds'
+        item.type = 'base_fields'
       })
+      // 判断服务器有无返回数据端点
       if (!this.dataList.datapoints) {
+        // 弱无返回数据端点 将标志位变为true
         needResetData = true
+        // 用获取数据端点接口返回的数据端点作为当前数据端点的默认值
         this.dataList.datapoints = this.datapoints
       }
-      let datapoints = this.dataList.datapoints.map((item) => {
-        let dataPointNames = this.datapoints.map((point) => point.name)
-        if (dataPointNames.indexOf(item.name) === -1) {
+      // 如果当前标志位仍然为false
+      if (!needResetData) {
+        if (this.dataList.datapoints.length === this.datapoints.length) {
+          var dArray = this.dArray(this.dataList.datapoints, this.datapoints, 'name')
+          if (dArray.length) {
+            needResetData = true
+          }
+        } else {
           needResetData = true
         }
-
-        let obj = {}
-        obj.name = item.name
-        obj.label = item.label || item.name
-        obj.hidden = item.hidden === false ? item.hidden : true
-        obj.sort = item.sort || 9
-        obj.show = !item.hidden
-        obj.type = 'datapoints'
-        return obj
+      }
+      // 初始化数据端点 用获取数据端点接口返回的数据端点去初始化当前的每个类型为数据端点的产品字段
+      let datapoints = []
+      this.datapoints.forEach((item1) => {
+        let hasPoints = false
+        this.dataList.datapoints.forEach((item2) => {
+          if (item1.name === item2.name) {
+            hasPoints = true
+            let obj = {}
+            obj.name = item2.name
+            obj.label = item2.label
+            obj.hidden = item2.hidden || false
+            obj.sort = item2.sort
+            obj.show = !obj.hidden
+            obj.type = 'datapoints'
+            obj.index = item2.index
+            datapoints.push(obj)
+          }
+        })
+        if (!hasPoints) {
+          let obj = {}
+          obj.name = item1.name
+          obj.label = item1.name
+          obj.hidden = true
+          obj.sort = item1.index + 9
+          obj.show = !obj.hidden
+          obj.type = 'datapoints'
+          obj.index = item1.index
+          datapoints.push(obj)
+        }
       })
-      result = this.dataList.base_fileds.concat(datapoints)
-      console.log(result)
+
+      // 将基本字段数组和数据端点数组合并成一个
+      result = this.dataList.base_fields.concat(datapoints)
+      // 用sort字段去排序
       result.sort((a, b) => {
         return a.sort - b.sort
       })
       this.fieldList = result
+      // 判断标志位needResetData 如果为true  则向服务器发请求重置字段列表
       if (needResetData) {
-        alert('不同')
-        // this.setFiled()
+        this.setFiled()
       }
     },
     /**
@@ -379,7 +407,13 @@ export default {
      * @return {[type]}      [description]
      */
     translate (type) {
-      return type === 'base_fileds' ? '基本字段' : '数据端点'
+      return type === 'base_fields' ? '基本字段' : '数据端点'
+    },
+
+    dArray (arr1, arr2, key) {
+      var result1 = _.differenceBy(arr1, arr2, key)
+      var result2 = _.differenceBy(arr2, arr1, key)
+      return result1.concat(result2)
     }
   }
 }
