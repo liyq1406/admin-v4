ce04a0906e1276532d23e37b864d78ba65bf9e11
FulukawaA <442873864@qq.com>
2016-09-19 18:19:32 +0800

    维保-添加维修工单

--- src/views/operation/plugin/warranty/work-order/repair/add.vue
+++ src/views/operation/plugin/warranty/work-order/repair/add.vue
@@ -8,23 +8,23 @@
       <div class="panel-bd">
         <div class="row">
           <div class="col-16 alert-max form">
-            <form  v-form name="addValidation" @submit.prevent="onSubmit" hook="addFormHook">
+            <form  v-form name="addValidation" @submit.prevent="onSubmit">
               <div class="form-row row">
                 <label class="form-control col-5 alert-label">设备SN:</label>
                 <div class="controls col-19">
                   <div v-placeholder="'请输入设备序列号'" class="input-text-wrap">
-                    <input v-model="addModal.sn" type="text" v-form-ctrl name="name" minlength="2" maxlength="32" lazy required class="input-text"/>
+                    <input v-model="addModal.product_sn" type="text" v-form-ctrl name="product_sn" minlength="2" maxlength="32" lazy required class="input-text"/>
                   </div>
-                  <div v-if="addValidation.$submitted && addValidation.name.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.name.$error.required">{{ $t('ui.validation.required', {field: $t('ui.rule.fields.name')}) }}</span></div>
+                  <div v-if="addValidation.$submitted && addValidation.product_sn.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.product_sn.$error.required">请输入设备SN</span></div>
                 </div>
               </div>
 
               <div class="form-row row">
-                <label class="form-control col-5 alert-label">设备类型:</label>
+                <label class="form-control col-5 alert-label">选择产品:</label>
                 <div class="controls col-19">
-                  <x-select :label="'空气净化器'" :width="'120px'">
-                    <select v-model="addModal.deviceType">
-                      <option v-for="type in deviceTypes" :value="type">{{ type.label }}</option>
+                  <x-select :label="selectedProduct.name" :width="'120px'">
+                    <select v-model="selectedProduct">
+                      <option v-for="product in products" :value="product">{{ product.name }}</option>
                     </select>
                   </x-select>
                 </div>
@@ -33,9 +33,9 @@
               <div class="form-row row">
                 <label class="form-control col-5 alert-label">维修类型:</label>
                 <div class="controls col-19">
-                  <x-select :label="'设备故障'" :width="'120px'">
-                    <select v-model="addModal.deviceType">
-                      <option v-for="type in deviceTypes" :value="type">{{ type.label }}</option>
+                  <x-select :label="selectedType" :width="'120px'">
+                    <select v-model="selectedType">
+                      <option v-for="type in types" :value="type">{{ type }}</option>
                     </select>
                   </x-select>
                 </div>
@@ -47,7 +47,7 @@
                   <div v-placeholder="'请输入维修内容'" class="input-text-wrap">
                     <input v-lengthtip="{max: 32, model:addModal.content }" v-model="addModal.content" type="text" v-form-ctrl name="name" minlength="0" maxlength="32" lazy required class="input-text"/>
                   </div>
-                  <div v-if="addValidation.$submitted && addValidation.name.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.name.$error.required">{{ $t('ui.validation.required', {field: $t('ui.rule.fields.name')}) }}</span></div>
+                  <div v-if="addValidation.$submitted && addValidation.name.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.name.$error.required">请输入维修内容</span></div>
                 </div>
               </div>
 
@@ -55,9 +55,9 @@
                 <label class="form-control col-5 alert-label">维修说明:</label>
                 <div class="controls col-19">
                   <div v-placeholder="'请输入维修说明'" class="input-text-wrap">
-                    <textarea v-lengthtip="{max: 240, model:addModal.discription }" v-model="addModal.sn" type="text" v-form-ctrl name="name" minlength="2" maxlength="32" lazy required class="input-text"></textarea>
+                    <textarea v-lengthtip="{max: 240, model:addModal.discription }" v-model="addModal.remark" type="text" v-form-ctrl name="remark" minlength="2" maxlength="32" lazy required class="input-text"></textarea>
                   </div>
-                  <div v-if="addValidation.$submitted && addValidation.name.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.name.$error.required">{{ $t('ui.validation.required', {field: $t('ui.rule.fields.name')}) }}</span></div>
+                  <div v-if="addValidation.$submitted && addValidation.remark.$pristine" class="form-tips form-tips-error"><span v-if="addValidation.remark.$error.required">请输入维修说明</span></div>
                 </div>
               </div>
 
@@ -69,11 +69,22 @@
               </div>
 
               <div class="form-row row">
+                <label class="form-control col-5 alert-label">网点选择:</label>
+                <div class="controls col-19">
+                  <x-select :label="selectedBranch.name" :width="'120px'">
+                    <select v-model="selectedBranch">
+                      <option v-for="branch in branchs" :value="branch">{{ branch.name }}</option>
+                    </select>
+                  </x-select>
+                </div>
+              </div>
+
+              <div class="form-row row">
                 <label class="form-control col-5 alert-label">处理人员:</label>
                 <div class="controls col-19">
-                  <x-select :label="'陈翔'" :width="'120px'">
-                    <select v-model="addModal.deviceType">
-                      <option v-for="type in deviceTypes" :value="type">{{ type.label }}</option>
+                  <x-select :label="selectedBranchStarff.name" :width="'120px'">
+                    <select v-model="selectedBranchStarff">
+                      <option v-for="staff in branchStaffs" :value="staff">{{ staff.name }}</option>
                     </select>
                   </x-select>
                 </div>
@@ -96,11 +107,18 @@
   import Select from 'components/Select'
   import ImageUploader from 'components/ImageUploader'
   import { globalMixins } from 'src/mixins'
+  import api from 'api'
 
   export default {
     name: 'add-warranty',
 
     mixins: [globalMixins],
+
+    vuex: {
+      getters: {
+        products: ({ products }) => products.released
+      }
+    },
 
     components: {
       Breadcrumb,
@@ -108,8 +126,33 @@
       'x-select': Select
     },
 
+    route: {
+      data () {
+        this.init()
+        this.getTags()
+        this.getBranchList()
+      }
+    },
+
+    watch: {
+      products () {
+        this.init()
+      },
+      types () {
+        this.getFirstType()
+      }
+    },
+
     data () {
       return {
+        adding: false,
+        branchStaffs: [],
+        selectedBranchStarff: {},
+        branchs: [],
+        selectedBranch: {},
+        types: [],
+        selectedType: '',
+        selectedProduct: {},
         addValidation: {},
         breadcrumbNav: [{
           label: '全部',
@@ -129,10 +172,83 @@
       }
     },
     methods: {
-      addFormHook (form) {
-        this.addModal.form = form
+      init () {
+        this.selectedProduct = this.products[0] || {}
+        // if (this.products.length > 0) {
+        //   this.getOrderWorkList()
+        // }
       },
-      onSubmit () {}
+      getFirstType () {
+        this.selectedType = this.types[0] || {}
+      },
+      // 获取标签
+      getTags () {
+        api.warranty.getWarrantyLabel(this.$route.params.app_id).then((res) => {
+          this.types = res.data.label
+        }).catch((res) => {
+          this.handleError(res)
+        })
+      },
+      // 获取网点列表
+      getBranchList () {
+        var params = {
+          filter: ['_id', 'name'],
+          limit: 100
+        }
+        api.warranty.getBranchList(this.$route.params.app_id, params).then((res) => {
+          this.branchs = res.data.list
+          this.selectedBranch = this.branchs[0] || {}
+          this.getBranchStaffsList()
+        }).catch((err) => {
+          this.handleError(err)
+        })
+      },
+      // 获取网点对应维修人员列表
+      getBranchStaffsList () {
+        console.log(888)
+        var params = {
+          filter: ['_id', 'name'],
+          limit: 100,
+          query: {
+            branch_id: this.selectedBranch._id
+          }
+        }
+        api.warranty.getBranchStaffsList(this.$route.params.app_id, params).then((res) => {
+          this.branchStaffs = res.data.list
+          this.selectedBranchStarff = this.branchStaffs[0] || {}
+        }).catch((err) => {
+          this.handleError(err)
+        })
+      },
+      // addFormHook (form) {
+      //   this.addModal.form = form
+      // },
+      // 提交添加
+      onSubmit () {
+        console.log(this.addValidation.$invalid)
+        if (this.adding) return
+
+        // if (this.$addValidation.invalid) {
+        //   this.$validate(true)
+        //   return
+        // }
+        if (this.addValidation.$valid) {
+          this.addModal.product_id = this.selectedProduct.id
+          this.addModal.label = this.selectedType
+          this.addModal.images = this.images
+          this.addModal.branch_id = this.selectedBranch._id
+          this.addModal.assigned_id = this.selectedBranchStarff._id
+          this.addModal.assigned_name = this.selectedBranchStarff.name
+          this.addModal.status = 0
+          this.adding = true
+          api.warranty.addRepairDetailList(this.$route.params.app_id, this.addModal).then((res) => {
+            this.adding = false
+            this.$route.router.go('/operation/plugins/warranty/' + this.$route.params.app_id + '/work-orders/repair')
+          }).catch((err) => {
+            this.handleError(err)
+          })
+        }
+      }
     }
   }
 </script>
