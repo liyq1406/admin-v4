c0353b51c2a5e9386d029e97ec51353487a2effa
FulukawaA <442873864@qq.com>
2016-09-27 17:53:16 +0800

    首页完成

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,132 +1,141 @@
 <template>
   <div class="page-in">
     <div class="body row">
-      <div class="dev-box container col-12">
-
-        <div class="part part1 base-introduce">
-          <div class="icon-box"></div>
-          <div class="text-box">
-            <h2>我是开发</h2>
-            <div class="description">在开发平台创建产品、开发调试产品，配置产品设备属性，发布产品上线</div>
-          </div>
-        </div>
-
-        <div class="part part2 product-list">
-          <div class="title-box">
-            <h2>产品开发</h2>
-            <a class="check-all" v-show="isReleaseProductsCount + noReleaseProductsCount" @click="goto('/dev')">查看全部({{isReleaseProductsCount + noReleaseProductsCount}}) ></a>
-          </div>
-          <div class="content-box">
-            <div class="product" v-for="product in devProducts">
-              <div class="img-box">
-                <img :src="product.pic">
-              </div>
+      <div class="tips">维护公告：今日9-23 04：00-05：00将进行系统升级！ </div>
+      <div class="row widmid">
+        <div class="dev-box container col-12">
+          <div class="container-body">
+            <div class="part part1 base-introduce">
+              <div class="icon-box"></div>
               <div class="text-box">
-                <div class="line msg">
-                  <a class="name" v-link="{'path': '/dev/products/' + product.id + '/info'}">{{product.name}}</a>
-                  <!-- <span class="product-id">({{product.id}})</span> -->
+                <h2>我是开发</h2>
+                <div class="description">在开发平台创建产品、开发调试产品，配置产品设备属性，发布产品上线</div>
+              </div>
+            </div>
+
+            <div class="part part2 product-list">
+              <div class="title-box">
+                <h2>产品开发</h2>
+                <a class="check-all" v-show="isReleaseProductsCount + noReleaseProductsCount" @click="goto('/dev')">查看全部({{isReleaseProductsCount + noReleaseProductsCount}}) ></a>
+              </div>
+              <div class="content-box">
+                <div class="product" v-for="product in devProducts">
+                  <div class="img-box">
+                    <img :src="product.pic">
+                  </div>
+                  <div class="text-box">
+                    <div class="line msg">
+                      <a class="name" v-link="{'path': '/dev/products/' + product.id + '/info'}">{{product.name}}</a>
+                      <!-- <span class="product-id">({{product.id}})</span> -->
+                    </div>
+                    <div class="info">
+                      <span>授权：{{product['quota'] || 0}}</span>
+                      <span class="ml10">| 设备数：{{product['total'] || '0'}}</span>
+                      <span class="ml10">| 状态：{{product['is_release']?'已发布':'未发布'}}</span>
+                    </div>
+                  </div>
                 </div>
-                <div class="info">
-                  <span>授权：{{product['quota'] || 0}}</span>
-                  <span class="ml10">| 设备数：{{product['total'] || '0'}}</span>
-                  <span class="ml10">| 状态：{{product['is_release']?'已发布':'未发布'}}</span>
+                <div class="no-products ml10 grayfont" v-show="devProducts.length === 0">
+                  <span>您还没有任何产品，请点击按钮开始创建</span>
+                  <button class="btn btn-success ml20 bradius" @click="goto('/dev/products/create')">
+                    <i class="fa fa-plus"></i>
+                    添加产品
+                  </button>
                 </div>
               </div>
             </div>
-            <div class="no-products ml10" v-show="devProducts.length === 0">
-              <span>您还没有任何产品，请点击按钮开始创建</span>
-              <button class="btn btn-success ml20 bradius" @click="goto('/dev/products/create')">
-                <i class="fa fa-plus"></i>
-                添加产品
-              </button>
-            </div>
-          </div>
-        </div>
 
-        <div class="part part3 link-box">
-          <div class="title-box">
-            <h2>开发指南</h2>
-            <!-- <a class="check-all">查看全部 ></a> -->
-          </div>
-          <div class="content-box">
-            <div class="link" v-for="link in links">
-              <i class="fa fa-file-text-o"></i>
-              <a @click="open(link.path)">{{link.content}}</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="part part4 entry">
-          <button class="btn btn-primary" @click="goto('/dev')">立即进入</button>
-        </div>
-
-      </div>
-      <div class="operation-box container col-12">
-        <div class="part part1 base-introduce">
-          <div class="icon-box"></div>
-          <div class="text-box">
-            <h2>我是运营</h2>
-            <div class="description">在运营平台查看设备数据、用户数据，查看设备运行状态并针对设备和用户数据统计运营数据</div>
-          </div>
-        </div>
-
-        <div class="part part2 product-list">
-          <div class="title-box">
-            <h2>产品管理</h2>
-            <a class="check-all" v-show="isReleaseProductsCount" @click="goto('/operation/overview')">查看全部({{isReleaseProductsCount}}) ></a>
-          </div>
-          <div class="content-box">
-            <div class="product" v-if="corp.status!==0 && releaseProducts.length!==0" v-for="product in releaseProducts">
-              <div class="img-box">
-                <img :src="product.pic">
+            <div class="part part3 link-box">
+              <div class="title-box">
+                <h2>开发指南</h2>
+                <a class="check-all">查看全部 ></a>
               </div>
+              <div class="content-box">
+                <div class="link" v-for="link in links">
+                  <i class="fa fa-file-text-o"></i>
+                  <a @click="open(link.path)">{{link.content}}</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="part part4 entry">
+              <button class="btn btn-primary bradius" @click="goto('/dev')">立即进入</button>
+            </div>
+          </div>
+
+        </div>
+        <div class="operation-box container col-12">
+          <div class="container-body">
+            <div class="part part1 base-introduce">
+              <div class="icon-box"></div>
               <div class="text-box">
-                <div class="line msg">
-                  <a class="name" v-link="{'path': '/operation/products/' + product.id + '/overview'}">{{product.name}}</a>
-                  <!-- <span class="product-id">({{product.id}})</span> -->
+                <h2>我是运营</h2>
+                <div class="description">在运营平台查看设备数据、用户数据，查看设备运行状态并针对设备和用户数据统计运营数据</div>
+              </div>
+            </div>
+
+            <div class="part part2 product-list">
+              <div class="title-box">
+                <h2>产品管理</h2>
+                <a class="check-all" v-show="isReleaseProductsCount" @click="goto('/operation/overview')">查看全部({{isReleaseProductsCount}}) ></a>
+              </div>
+              <div class="content-box">
+                <div class="product" v-if="corp.status!==0 && releaseProducts.length!==0" v-for="product in releaseProducts">
+                  <div class="img-box">
+                    <img :src="product.pic">
+                  </div>
+                  <div class="text-box">
+                    <div class="line msg">
+                      <a class="name" v-link="{'path': '/operation/products/' + product.id + '/overview'}">{{product.name}}</a>
+                      <!-- <span class="product-id">({{product.id}})</span> -->
+                    </div>
+                    <div class="info">
+                      <span>设备数：{{product['total']}}</span>
+                      <span class="ml10">| 激活：{{product['activated']}}</span>
+                      <span class="ml10">| 在线：{{product['online']}}</span>
+                    </div>
+                  </div>
                 </div>
-                <div class="info">
-                  <span>设备数：{{product['total']}}</span>
-                  <span class="ml10">| 激活：{{product['activated']}}</span>
-                  <span class="ml10">| 在线：{{product['online']}}</span>
+                <div class="no-products ml10 grayfont" v-show="corp.status===0">
+                  <span>您的账号目前尚未认证，待认证成功后，即可使用运营平台，任何疑问联系：400-291-234</span>
+                </div>
+                <div class="no-products ml10 grayfont" v-show="corp.status!==0 && releaseProducts.length===0">
+                  <span>暂未发布产品，马上去开发平台发布一款产品，即可使用运营平台，任何疑问联系：400-291-234</span>
                 </div>
               </div>
             </div>
-            <div class="no-products ml10" v-show="corp.status===0">
-              <span>您的账号目前尚未认证，待认证成功后，即可使用运营平台，任何疑问联系：400-291-234</span>
+
+            <div class="part part3 link">
+              <div class="title-box">
+                <h2>快捷导航</h2>
+                <!-- <a class="check-all" @click="goto('/operation/overview')">查看全部 ></a> -->
+              </div>
+              <div class="content-box">
+                <div class="nav">
+                  <span>产品管理：</span>
+                  <a v-for="nav in nav.products" @click="goto(nav.path)">{{nav.content}}</a>
+                </div>
+                <div class="nav">
+                  <span>告警服务：</span>
+                  <a v-for="nav in nav.alerts" @click="goto(nav.path)">{{nav.content}}</a>
+                </div>
+                <div class="nav">
+                  <span>用户管理：</span>
+                  <a v-for="nav in nav.users" @click="goto(nav.path)">{{nav.content}}</a>
+                </div>
+              </div>
             </div>
-            <div class="no-products ml10" v-show="corp.status!==0 && releaseProducts.length===0">
-              <span>暂未发布产品，马上去开发平台发布一款产品，即可使用运营平台，任何疑问联系：400-291-234</span>
+
+            <div class="part part4 entry">
+              <button class="btn btn-primary bradius" @click="goto('/operation/overview')">立即进入</button>
             </div>
           </div>
         </div>
-
-        <div class="part part3 link">
-          <div class="title-box">
-            <h2>快捷导航</h2>
-            <!-- <a class="check-all" @click="goto('/operation/overview')">查看全部 ></a> -->
-          </div>
-          <div class="content-box">
-            <div class="nav">
-              <span>产品管理：</span>
-              <a v-for="nav in nav.products" @click="goto(nav.path)">{{nav.content}}</a>
-            </div>
-            <div class="nav">
-              <span>告警服务：</span>
-              <a v-for="nav in nav.alerts" @click="goto(nav.path)">{{nav.content}}</a>
-            </div>
-            <div class="nav">
-              <span>用户管理：</span>
-              <a v-for="nav in nav.users" @click="goto(nav.path)">{{nav.content}}</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="part part4 entry">
-          <button class="btn btn-primary" @click="goto('/operation/overview')">立即进入</button>
-        </div>
-
       </div>
+
+    </div>
+    <div class="footer-banner">
+      <img src="/static/images/banner.jpg" alt="">
     </div>
     <div class="footer">
       <span>© 2016  云智易 |  www.xlink.cn</span>
@@ -339,9 +348,21 @@ export default {
 
 <style lang="stylus" scoped>
 @import '../assets/stylus/common'
+  .widmid
+    width 1310px
+    margin 0 auto
+  .tips
+    /*height 25px*/
+    line-height 25px
+    padding 5px 10px
+    box-sizing border-box
+    background-color #fafafa
+    color #da4e37
+    margin-bottom 20px
   .bradius
     border-radius 10px
-
+  .grayfont
+    color #bcbcbc
   .page-in
     width 100%
     height 100%
@@ -356,12 +377,17 @@ export default {
       box-sizing border-box
       overflow auto
       .dev-box
-        border-right 1px solid #dddddd
+        /*border-right 1px solid #dddddd*/
         box-sizing border-box
 
     .container
-      padding 0 80px 20px
+      /*padding 0 80px 20px*/
       box-sizing border-box
+      .container-body
+        margin 0 20px 10px
+        padding 10px 20px
+        border 1px solid #dddddd
+        border-radius 10px
       &.dev-box
         .icon-box
           background url('../assets/images/developer.png') no-repeat center center / 90% 90%
@@ -396,26 +422,28 @@ export default {
           box-sizing border-box
           width 100%
       .part1
+        text-align center
         position relative
         min-height 85px
         .icon-box
           width 85px
           height 85px
-          position absolute
+          /*position absolute
           top 0
-          left 0
-
+          left 0*/
+          margin 0 auto
         .text-box
-          margin-left 100px
+          /*margin-left 100px*/
           padding-top 6px
           h2
             margin-top 10px
             font-size 25px
             padding 0
-            margin 0
+            margin 0 0 20px 0
             color #333333
           .description
             color #bcbcbc
+            margin-bottom 60px
       .part2
         .product
           width 100%
@@ -476,6 +504,13 @@ export default {
           height 33px
           background-color #da4e37
           font-size 14px
+    .footer-banner
+      width 100%
+      height 100px
+      position absolute
+      left 0
+      bottom 100px
+      text-align center
 
     .footer
       width 100%
@@ -484,7 +519,7 @@ export default {
       text-align center
       background #f6f6f6
       color #999
-      border-top 1px solid #dddddd
+      /*border-top 1px solid #dddddd*/
       border-bottom 1px solid #dddddd
       position absolute
       bottom 53px
