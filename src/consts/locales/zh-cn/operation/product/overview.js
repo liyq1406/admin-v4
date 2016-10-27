9cb3fa99667eeca58708f80e442aa9f4bd9f1fb8
ShighGoing <shigh945@gmail.com>
2016-10-27 16:51:08 +0800

    增加产品下翻译的目录文件

---
+++ src/consts/locales/zh-cn/operation/product/overview.js
@@ -0,0 +1,33 @@
+/**
+ * 产品概览页翻译信息
+ * @return {Object}
+ */
+
+export default {
+  main_title: '概览',
+  total: {
+    count: '设备总数', // 设备总数
+    tooltip: '当前产品的设备总量' // 企业的设备总量
+  },
+  activated: {
+    count: '激活设备数', // 激活数
+    tooltip: '已联网激活的设备数量' // 已联网激活的设备数量
+  },
+  online: {
+    count: '在线设备数', // 在线设备数
+    tooltip: '当前在线的设备数量' // 当前在线的设备数量
+  },
+  users: {
+    count: '用户总数', // 用户总数
+    tooltip: '当前产品的用户数量' // 当前产品的用户数量
+  },
+  trend: {
+    title: '产品趋势',
+    today_add: '今日增长'
+  },
+  active: {
+    title: '产品活跃度',
+    not_active_device: '未激活设备',
+    active_device: '激活设备'
+  }
+}
