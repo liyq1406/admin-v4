fc41944d8ce241d348eaa268f5eb831c70f17a9a
ShighGoing <shigh945@gmail.com>
2016-11-02 10:15:05 +0800

    修改自定义配置

--- src/views/dev/settings/views/overview/index.vue
+++ src/views/dev/settings/views/overview/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="main">
+  <div class="panel">
+    <div class="panel-bd">
+      <div class="form-row row">
+        <div class="form-control col-4">
+          <label>选择产品:</label>
+        </div>
+        <div class="controls col-20">
+          <div v-for="i in 12" class="fl input-radio-wrap">
+            <input v-model="" type="radio" :value="0" name="connect-type"/>
+            <label>一个产品</label>
+          </div>
+        </div>
+      </div>
+      <div class="form-row row">
+        <div class="form-control col-4">
+          <label>数据指标配置:</label>
+        </div>
+        <div class="controls col-20">
+          <div class="radio-button-wrap">
+            <radio-button-group :items="periodsInfo" @select="periodSelect"></radio-button-group>
+          </div>
+        </div>
+      </div>
+      <div class="form-row row">
+        <div class="form-control col-4">
+          <label>功能模块:</label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,9 +48,20 @@ export default {
   data () {
     return {
     }
+  },
+  methods: {
+    periodSelect () {}
   }
 }
 </script>
 
 <style lang="stylus">
+.input-radio-wrap
+  position relative
+  line-height 30px
+  display inline-block
+  input, label
+    vertical-align middle
+  label
+    margin-right 20px
 </style>
