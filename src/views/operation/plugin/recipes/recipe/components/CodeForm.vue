ebdf9d63871c7d1626b1b461f92a7962bf5d9fe9
FulukawaA <442873864@qq.com>
2016-11-02 11:38:49 +0800

    修改树装组件表单初始化

--- src/views/operation/plugin/recipes/recipe/components/CodeForm.vue
+++ src/views/operation/plugin/recipes/recipe/components/CodeForm.vue
@@ -5,7 +5,7 @@
         <label class="form-control col-6">参数名称:</label>
         <div class="controls col-18">
           <div class="input-text-wrap">
-            <input v-model="menu.name" name="menu.type" type="text" class="input-text" v-validate:name="{required: true, maxlength: 20, format: 'trim'}">
+            <input v-model="cloneMenu.name" name="cloneMenu.type" type="text" class="input-text" v-validate:name="{required: true, maxlength: 20, format: 'trim'}">
           </div>
           <div class="form-tips form-tips-error">
             <span v-if="$validation.name.touched && $validation.name.required">请输入参数名称</span>
@@ -18,7 +18,7 @@
         <label class="form-control col-6">参数ID:</label>
         <div class="controls col-18">
           <div class="input-text-wrap">
-            <input v-model="menu.param_id" name="menu.param_id" type="text" class="input-text" v-validate:id="{required: true, maxlength: 20, format: 'trim'}">
+            <input v-model="cloneMenu.param_id" name="cloneMenu.param_id" type="text" class="input-text" v-validate:id="{required: true, maxlength: 20, format: 'trim'}">
           </div>
           <div class="form-tips form-tips-error">
             <span v-if="$validation.id.touched && $validation.id.required">请输入参数ID</span>
@@ -31,7 +31,7 @@
         <label class="form-control col-6">参数信息:</label>
         <div class="controls col-18">
           <div class="input-text-wrap">
-            <input v-model="menu.desc" name="menu.name" type="text" class="input-text" v-validate:desc="{required: true, maxlength: 20, format: 'trim'}">
+            <input v-model="cloneMenu.desc" name="cloneMenu.name" type="text" class="input-text" v-validate:desc="{required: true, maxlength: 20, format: 'trim'}">
           </div>
           <div class="form-tips form-tips-error">
             <span v-if="$validation.desc.touched && $validation.desc.required">请输入参数信息</span>
@@ -44,7 +44,7 @@
         <label v-if="type==='edit'" class="del-check">
           <input type="checkbox" name="del" v-model="delChecked"/> 删除此参数
         </label>
-        <button @click.prevent.stop="handleSubmit(menu)" class="btn btn-primary">确定</button>
+        <button @click.prevent.stop="handleSubmit(cloneMenu)" class="btn btn-primary">确定</button>
         <button @click.prevent.stop="cancel" class="btn btn-default">{{ $t("common.cancel") }}</button>
       </div>
     </form>
@@ -53,7 +53,7 @@
 
 <script>
 import Modal from 'components/Modal'
-// import _ from 'lodash'
+import _ from 'lodash'
 import { globalMixins } from 'src/mixins'
 import { pluginMixins } from '../../../mixins'
 
@@ -81,7 +81,8 @@ export default {
 
   data () {
     return {
-      delChecked: false
+      delChecked: false,
+      cloneMenu: {}
       // orignModel: {
       //   type: ''
       // }
@@ -92,19 +93,27 @@ export default {
   ready () {
     // this.orignModel.type = this.menu.type
     this.$resetValidation()
+    this.init()
+  },
+  route () {
+    this.$resetValidation()
   },
 
   methods: {
-    handleSubmit (menu) {
+    // 初始化
+    init () {
+      this.cloneMenu = _.clone(this.menu)
+    },
+    handleSubmit (cloneMenu) {
       if (this.$validation.invalid) {
         return
       }
-
+      this.menu = this.cloneMenu
       if (this.delChecked === false) {
-        this.$emit('submit', menu)
+        this.$emit('submit', this.menu)
         this.$resetValidation()
       } else if (this.delChecked === true) {
-        this.$emit('delete', menu)
+        this.$emit('delete', this.menu)
         this.$resetValidation()
       }
     },
--- src/views/operation/plugin/recipes/recipe/components/MenuForm.vue
+++ src/views/operation/plugin/recipes/recipe/components/MenuForm.vue
@@ -5,11 +5,11 @@
         <label class="form-control col-6">菜单名称:</label>
         <div class="controls col-18">
           <div class="input-text-wrap">
-            <input v-model="menu.type" name="menu.type" type="text" class="input-text" v-validate:chimenu="{required: true, maxlength: 20, format: 'trim'}" >
+            <input v-model="cloneMenu.type" name="cloneMenu.type" type="text" class="input-text" v-validate:chimenu="{required: true, maxlength: 10, format: 'trim'}" >
           </div>
           <div class="form-tips form-tips-error">
             <span v-if="$validation.chimenu.touched && $validation.chimenu.required">请输入菜单名称</span>
-            <span v-if="$validation.chimenu.modified && $validation.chimenu.maxlength">菜单名称不能超过20位</span>
+            <span v-if="$validation.chimenu.modified && $validation.chimenu.maxlength">菜单名称不能超过10位</span>
             <span v-if="$validation.chimenu.touched && $validation.chimenu.format">菜单名称不允许前后带空格</span>
           </div>
         </div>
@@ -18,7 +18,7 @@
         <label v-if="type==='edit'" class="del-check">
           <input type="checkbox" name="del" v-model="delChecked"/> 删除此菜单
         </label>
-        <button @click.prevent.stop="handleSubmit(menu)" class="btn btn-primary">确定</button>
+        <button @click.prevent.stop="handleSubmit(cloneMenu)" class="btn btn-primary">确定</button>
         <button @click.prevent.stop="cancel" class="btn btn-default">{{ $t("common.cancel") }}</button>
       </div>
     </form>
@@ -27,7 +27,7 @@
 
 <script>
 import Modal from 'components/Modal'
-// import _ from 'lodash'
+import _ from 'lodash'
 import { globalMixins } from 'src/mixins'
 import { pluginMixins } from '../../../mixins'
 
@@ -55,7 +55,8 @@ export default {
 
   data () {
     return {
-      delChecked: false
+      delChecked: false,
+      cloneMenu: {}
       // orignModel: {
       //   type: ''
       // }
@@ -66,18 +67,25 @@ export default {
   ready () {
     // this.orignModel.type = this.menu.type
     this.$resetValidation()
+    this.init()
   },
 
   methods: {
-    handleSubmit (menu) {
+    // 初始化
+    init () {
+      this.cloneMenu = _.clone(this.menu)
+      // console.log('cloneMenu:' + JSON.stringify(this.cloneMenu))
+    },
+    handleSubmit (cloneMenu) {
       if (this.$validation.invalid) {
         return
       }
+      this.menu = this.cloneMenu
       if (this.delChecked === false) {
-        this.$emit('submit', menu)
+        this.$emit('submit', this.menu)
         this.$resetValidation()
       } else if (this.delChecked === true) {
-        this.$emit('delete', menu)
+        this.$emit('delete', this.menu)
         this.$resetValidation()
       }
     },
