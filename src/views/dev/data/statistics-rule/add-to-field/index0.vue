e4e2f447132d02f99165e2874ea0db5746bea133
邱国辉 <530784837@qq.com>
2016-11-25 11:37:29 +0800

    将 统计规则添加到自定义字段 页面的选择快照改成统计规则

--- src/views/dev/data/statistics-rule/add-to-field/index0.vue
+++ src/views/dev/data/statistics-rule/add-to-field/index0.vue
@@ -48,20 +48,18 @@
                 </div>
               </div>
             </div>
-
             <div class="form-row row">
-              <label class="form-control col-3">统计规则:
+              <label class="form-control col-3">快照名称:
                 <i class="fa fa-question-circle" v-tooltip="'快照类型必须是 变化快照 或者 即时快照'"></i>
               </label>
               <div class="controls col-21">
-                <x-select :label="selectedSnapshotRule.label" width="200px">
-                  <select v-model="selectedSnapshotRule" :disabled="type==='edit'">
+                <x-select :label="selectedSnapshot.label" width="200px" :disabled="type==='edit'">
+                  <select v-model="selectedSnapshot" :disabled="type==='edit'">
                     <option v-for="opt in snapshotOptions" :value="opt">{{ opt.label }}</option>
                   </select>
                 </x-select>
               </div>
             </div>
-
             <div class="form-row row">
               <label class="form-control col-3">时间粒度:
                 <i class="fa fa-question-circle" v-tooltip="'选择时间粒度，可根据所选时间，查看以该时间为粒度的数据统计分析，不可多选'"></i>
@@ -69,16 +67,16 @@
               <div class="controls col-21 fineness-select mutiple-select">
                 <!-- <input v-model="fineness" type="radio" name="fineness" value="hour">
                 <label>小时</label> -->
-                <input v-model="fineness" type="radio" name="fineness" value="day" :disabled="canSelectFineness.indexOf(2) === -1">
-                <label :class="{'disabled': canSelectFineness.indexOf(2) === -1}">天</label>
-                <input v-model="fineness" type="radio" name="fineness" value="week" :disabled="canSelectFineness.indexOf(3) === -1">
-                <label :class="{'disabled': canSelectFineness.indexOf(3) === -1}">周</label>
-                <input v-model="fineness" type="radio" name="fineness" value="month" :disabled="canSelectFineness.indexOf(4) === -1">
-                <label :class="{'disabled': canSelectFineness.indexOf(4) === -1}">月</label>
-                <input v-model="fineness" type="radio" name="fineness" value="year" :disabled="canSelectFineness.indexOf(5) === -1">
-                <label :class="{'disabled': canSelectFineness.indexOf(5) === -1}">年</label>
-                <input v-model="fineness" type="radio" name="fineness" value="all" :disabled="canSelectFineness.indexOf(6) === -1">
-                <label :class="{'disabled': canSelectFineness.indexOf(6) === -1}">全部</label>
+                <input v-model="fineness" type="radio" name="fineness" value="day">
+                <label>天</label>
+                <input v-model="fineness" type="radio" name="fineness" value="week">
+                <label>周</label>
+                <input v-model="fineness" type="radio" name="fineness" value="month">
+                <label>月</label>
+                <input v-model="fineness" type="radio" name="fineness" value="year">
+                <label>年</label>
+                <input v-model="fineness" type="radio" name="fineness" value="all">
+                <label>全部</label>
 
                 <span class="select-time" v-if="fineness && (fineness !== 'all')">
                   <x-select :label="timeText(fineness, selectTimeType)" width="100px" :disabled="type==='edit'">
@@ -90,7 +88,6 @@
                 </span>
               </div>
             </div>
-
             <div class="form-row row">
               <label class="form-control col-3">统计维度:
                 <i class="fa fa-question-circle" v-tooltip="'根据数据端点，选择统计维度，可以统计求和、平均、最大、最小，可多选'"></i>
@@ -117,39 +114,37 @@
                           <td>{{dp.index}}</td>
                           <td>{{dp.name}}</td>
                           <td class="mutiple-select">
-                            <template v-for="opt in snapshotTypeOptions">
-                              <input
-                              v-model="dp.statisticsType"
-                              type="radio"
-                              :name="dp.index"
-                              :value="opt.value"
-                              :disabled="!(selectedDataPoint.index === dp.index && dp.dpModes.indexOf(opt.code) >= 0)">
-                              <label :class="{'disabled': !(selectedDataPoint.index === dp.index && dp.dpModes.indexOf(opt.code) >= 0)}">{{opt.label}}</label>
-                            </template>
+                            <input
+                            v-model="dp.statisticsType"
+                            type="radio"
+                            :name="dp.index"
+                            value="avg"
+                            :disabled="selectedDataPoint.index !== dp.index">
+                            <label>平均</label>
 
-                            <!-- <input
+                            <input
                             v-model="dp.statisticsType"
                             type="radio"
                             :name="dp.index"
                             value="sum"
-                            :disabled="selectedDataPoint.index !== dp.index && dp.dpModes.indexOf(dp.index) === -1">
-                            <label :class="{'disabled': selectedDataPoint.index !== dp.index && dp.dpModes.indexOf(dp.index) === -1}">求和</label>
+                            :disabled="selectedDataPoint.index !== dp.index">
+                            <label>求和</label>
 
                             <input
                             v-model="dp.statisticsType"
                             type="radio"
                             :name="dp.index"
                             value="max"
-                            :disabled="selectedDataPoint.index !== dp.index && dp.dpModes.indexOf(dp.index) === -1">
-                            <label :class="{'disabled': selectedDataPoint.index !== dp.index && dp.dpModes.indexOf(dp.index) === -1}">最大</label>
+                            :disabled="selectedDataPoint.index !== dp.index">
+                            <label>最大</label>
 
                             <input
                             v-model="dp.statisticsType"
                             type="radio"
                             :name="dp.index"
                             value="min"
-                            :disabled="selectedDataPoint.index !== dp.index && dp.dpModes.indexOf(dp.index) === -1">
-                            <label :class="{'disabled': selectedDataPoint.index !== dp.index && dp.dpModes.indexOf(dp.index) === -1}">最小</label> -->
+                            :disabled="selectedDataPoint.index !== dp.index">
+                            <label>最小</label>
                           </td>
                         </tr>
                       </tbody>
@@ -161,10 +156,9 @@
                 </div>
               </div>
             </div>
-
             <div class="form-actions row">
               <div class="col-21 col-offset-3">
-                <button :disabled="disabled" :class="{'disabled':disabled}" class="btn btn-primary" @click.prevent="onSubmit">{{ $t('common.ok') }}</button>
+                <button :disabled="submitting || $validation.invalid || (selectedSnapshot && !selectedSnapshot.id) || !fineness.length || !isSelected" :class="{'disabled':submitting || $validation.invalid || (selectedSnapshot && !selectedSnapshot.id) || !fineness.length || !isSelected}" class="btn btn-primary" @click.prevent="onSubmit">{{ $t('common.ok') }}</button>
               </div>
             </div>
           </div>
@@ -204,32 +198,10 @@ export default {
 
   data () {
     return {
-      snapshotTypeOptions: [
-        {
-          value: 'max',
-          label: '最大',
-          code: 1
-        },
-        {
-          value: 'min',
-          label: '最小',
-          code: 2
-        },
-        {
-          value: 'avg',
-          label: '平均',
-          code: 3
-        },
-        {
-          value: 'sum',
-          label: '求和',
-          code: 4
-        }
-      ],
       fineness: '',
-      selectedSnapshotRule: {
+      selectedSnapshot: {
         id: 0,
-        label: '请选择快照规则'
+        label: '请选择快照'
       },
       name: '', // 规则key
       label: '', // 规则名称
@@ -240,7 +212,6 @@ export default {
       delChecked: false,
       loadingData: false,
       snapshotsRules: [],
-      statisticsRules: [],
       datapoints: [],
       curSelect: 0, // 为触发computer 无意义
       selectedDataPoint: {},
@@ -253,8 +224,7 @@ export default {
       let params = {
         offset: 0,
         limit: 10000, // 取所有规则
-        product_id: [],
-        type: 1
+        product_id: []
       }
       if (this.products && this.products.length) {
         this.products.forEach((item) => {
@@ -263,99 +233,71 @@ export default {
       }
       return params
     },
-
-    canSelectFineness () {
-      var result = this.selectedSnapshotRule.fineness || []
-      return result
-    },
-
-    disabled () {
-      return this.submitting || this.$validation.invalid || (this.selectedSnapshotRule && !this.selectedSnapshotRule.id) || !this.fineness.length || !this.selectedDataPoint.statisticsType
-    },
-
-    // 下拉框选项
+    // 产品选项
     snapshotOptions () {
-      var result = [
-        {
-          id: 0,
-          label: '请选择快照规则'
+      let result = this.snapshotsRules.map((item) => {
+        return {
+          id: item.id,
+          label: item.name,
+          datapoint: item.datapoint,
+          productId: item.productId
         }
-      ]
-      this.statisticsRules.forEach((item) => {
-        var option = _.clone(item)
-        option.label = item.name
-        result.push(option)
       })
+      result.unshift({
+        id: 0,
+        label: '请选择快照'
+      })
+
       return result
+    },
+    isSelected () {
+      return this.selectedDataPoint && this.selectedDataPoint.statisticsType
     }
   },
 
   watch: {
-    products () {
-      if (this.products && this.products.length) {
-        this.getStatisticRules()
+    selectedSnapshot () {
+      if (this.selectedSnapshot && this.selectedSnapshot.productId) {
+        this.getDatapoints()
+      } else {
+        this.datapoints = []
       }
     },
-    selectedSnapshotRule (val) {
-      this.selectedDataPoint = {}
-      this.getDatapoints()
+
+    products () {
+      this.getSnapshotRules()
     }
   },
 
-  route: {
-    data () {
-      if (this.products && this.products.length) {
-        this.getStatisticRules()
-      }
-    }
+  ready () {
+    this.getSnapshotRules()
   },
 
   methods: {
-    /**
-     * 获取统计快照规则
-     * @author guohao
-     */
-    getStatisticRules () {
-      api.snapshot.getAllStatisticRules(this.srQueryCondition).then((res) => {
-        if (res.status === 200 && res.data.list && res.data.list.length) {
-          res.data.list.sort((a, b) => {
-            a = +new Date(a.create_time)
-            b = +new Date(b.create_time)
-            return a - b
-          })
-          this.statisticsRules = res.data.list
-        } else {
-          this.statisticsRules = []
-        }
-      }).catch((res) => {
-        this.handleError(res)
-      })
-    },
-
     canBeCounted (type) {
       return (type >= 2 && type <= 5) || type === 8 || type === 9 // 可统计的数据端点类型
     },
-    // getSnapshotRules () {
-    //   this.snapshotsRules = []
-    //   let finished = 0
-    //   this.products.forEach((product) => {
-    //     api.snapshot.getRules(product.id).then((res) => {
-    //       finished++
-    //       if (res.status === 200 && res.data.list && res.data.list.length) {
-    //         res.data.list = _.filter(res.data.list, (item) => {
-    //           return item.rule === 1 || item.rule === 2
-    //         })
-    //         res.data.list.forEach((item) => {
-    //           item.productId = product.id
-    //         })
-    //         this.snapshotsRules = this.snapshotsRules.concat(res.data.list)
-    //       }
-    //     }).catch((res) => {
-    //       finished++
-    //       this.handleError(res)
-    //     })
-    //   })
-    // },
+    getSnapshotRules () {
+      this.snapshotsRules = []
+      let finished = 0
+      this.products.forEach((product) => {
+        api.snapshot.getRules(product.id).then((res) => {
+          finished++
+          if (res.status === 200 && res.data.list && res.data.list.length) {
+            res.data.list = _.filter(res.data.list, (item) => {
+              return item.rule === 1 || item.rule === 2
+            })
+            res.data.list.forEach((item) => {
+              item.productId = product.id
+            })
+            this.snapshotsRules = this.snapshotsRules.concat(res.data.list)
+          }
+        }).catch((res) => {
+          finished++
+          this.handleError(res)
+        })
+      })
+    },
 
     /**
      * 处理翻页
@@ -373,7 +315,7 @@ export default {
     getDatapoints () {
       this.loadingData = true
       // 获取产品数据端点列表
-      api.product.getDatapoints(this.selectedSnapshotRule.product_id).then((res) => {
+      api.product.getDatapoints(this.selectedSnapshot.productId).then((res) => {
         if (res.status === 200) {
           this.loadingData = false
           this.currentPage = 1
@@ -393,29 +335,26 @@ export default {
       let result = []
       datapoints.forEach((item) => {
         let find = false
-        var dpModes = []
-        if (this.selectedSnapshotRule.dp_mode && this.selectedSnapshotRule.dp_mode.length) {
-          this.selectedSnapshotRule.dp_mode.forEach((dpMode) => {
-            if (dpMode.index === item.index) {
+        if (this.selectedSnapshot.datapoint && this.selectedSnapshot.datapoint.length) {
+          this.selectedSnapshot.datapoint.forEach((index) => {
+            if (index === item.index) {
               find = true
-              dpModes.push(dpMode.mode)
             }
           })
         }
         if (find) {
-          item.dpModes = dpModes
           result.push(item)
         }
       })
+
       result = _.filter(result, (item) => {
         return this.canBeCounted(item.type)
       })
       result = result.map((item) => {
         item.statisticsType = ''
+        item.selected = false
         return item
       })
-      console.log('datapoint')
-      console.log(_.cloneDeep(result))
       return result
     },
 
@@ -436,7 +375,7 @@ export default {
         label: this.label,
         hidden: true,
         sort: 1000,
-        snapshot: this.selectedSnapshotRule.id,
+        snapshot: this.selectedSnapshot.id,
         datapointIndex: this.selectedDataPoint.index,
         dp_mode: this.getDpMode(this.selectedDataPoint.statisticsType),
         fineness: this.getFineness(),
@@ -445,8 +384,7 @@ export default {
       }
 
       this.submitting = true
-      console.log(this.selectedSnapshotRule.productId)
-      api.custom.field.getCustomFieldConfig(this.selectedSnapshotRule.product_id).then((data) => {
+      api.custom.field.getCustomFieldConfig(this.selectedSnapshot.productId).then((data) => {
         var params = data || {}
         params.base_fields = params.base_fields || []
         params.datapoints = params.datapoints || []
@@ -457,6 +395,7 @@ export default {
             keyRepeat = true
           }
         })
+        console.log(123)
         if (keyRepeat) {
           this.submitting = false
           return this.showNotice({
@@ -465,7 +404,7 @@ export default {
           })
         }
         params.snapshot_shuffle.push(model)
-        api.custom.field.setCustomFieldConfig(this.selectedSnapshotRule.product_id, params).then((data) => {
+        api.custom.field.setCustomFieldConfig(this.selectedSnapshot.productId, params).then((data) => {
           this.submitting = false
           this.showNotice({
             type: 'success',
@@ -517,8 +456,6 @@ export default {
   .select-time
     margin-left 15px
     display inline-block
-  .disabled
-    opacity 0.3
 .fineness-select
   display inline-block
   line-height 32px
